815686cba608d0dc7edfcfd4163b5e59f82a1a2c
pokamest <pokamest@gmail.com>
2021-06-02 18:41:08 +0300

    Travis fix

--- deploy/build_windows.sh
+++ deploy/build_windows.sh
@@ -4,8 +4,8 @@ echo "Build script started ..."
 
 echo -e 'call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\Common7\\Tools\\VsDevCmd.bat"\n' > winbuild.bat
 echo -e 'set BUILD_ARCH="64"\n' >> winbuild.bat
-echo -e 'set QT_BIN_DIR="c:\\Qt\\$QT_VERSION\\msvc2017_64\\bin"\n' >> winbuild.bat
-echo -e 'set QIF_BIN_DIR="c:\\Qt\\Tools\\QtInstallerFramework\\4.1\\bin"\n' >> winbuild.bat
+echo -e 'set QT_BIN_DIR="$QT_BIN_DIR"\n' >> winbuild.bat
+echo -e 'set QIF_BIN_DIR="QIF_BIN_DIR"\n' >> winbuild.bat
 echo -e 'call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Auxiliary\\Build\\vcvars%BUILD_ARCH:"=%.bat"\n' >> winbuild.bat
 cat winbuild.bat
 echo -e 'set WIN_CERT_PW=$WIN_CERT_PW\n' >> winbuild.bat
