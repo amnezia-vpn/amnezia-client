52450ef2f50ee73811fc7c19b61c65a4d712eb94
pokamest <pokamest@gmail.com>
2021-06-17 01:04:09 +0300

    Release 1.8

--- client/defines.h
+++ client/defines.h
@@ -4,7 +4,7 @@
 #define APPLICATION_NAME    "AmneziaVPN"
 #define SERVICE_NAME        "AmneziaVPN-service"
 #define ORGANIZATION_NAME   "AmneziaVPN.ORG"
-#define APP_MAJOR_VERSION   "1.7.6"
-#define APP_VERSION         "1.7.6.1"
+#define APP_MAJOR_VERSION   "1.8.0"
+#define APP_VERSION         "1.8.0.0"
 
 #endif // DEFINES_H
