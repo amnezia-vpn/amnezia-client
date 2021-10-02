a5bcf1a02dd9e634ea29b60b6fcdd21c573179df
pokamest <pokamest@gmail.com>
2021-10-02 21:56:47 +0300

    VPN configuration class fixes

--- client/configurators/vpn_configurator.cpp
+++ client/configurators/vpn_configurator.cpp
@@ -2,7 +2,7 @@
 #include "openvpn_configurator.h"
 #include "cloak_configurator.h"
 #include "shadowsocks_configurator.h"
-//#include "wireguard_configurator.h"
+#include "wireguard_configurator.h"
 
 #include <QFile>
 #include <QJsonObject>
@@ -24,12 +24,28 @@ QString VpnConfigurator::genVpnProtocolConfig(const ServerCredentials &credentia
     case Protocol::Cloak:
         return CloakConfigurator::genCloakConfig(credentials, container, containerConfig, errorCode);
 
-//    case Protocol::WireGuard:
-//        return WireGuardConfigurator::genWireGuardConfig(credentials, container, containerConfig, errorCode);
+    case Protocol::WireGuard:
+        return WireguardConfigurator::genWireguardConfig(credentials, container, containerConfig, errorCode);
 
     default:
         return "";
     }
+}
+
+QString VpnConfigurator::processConfigWithLocalSettings(DockerContainer container, Protocol proto, QString config)
+{
+    if (proto == Protocol::OpenVpn) {
+        return OpenVpnConfigurator::processConfigWithLocalSettings(config);
+    }
+    return config;
+}
+
+QString VpnConfigurator::processConfigWithExportSettings(DockerContainer container, Protocol proto, QString config)
+{
+    if (proto == Protocol::OpenVpn) {
+        return OpenVpnConfigurator::processConfigWithExportSettings(config);
+    }
+    return config;
 }
 
 void VpnConfigurator::updateContainerConfigAfterInstallation(DockerContainer container, QJsonObject &containerConfig,
