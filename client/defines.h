9cfc65eeda920b0e467e849501b059129407d1b7
pokamest <pokamest@gmail.com>
2022-09-11 00:24:43 +0300

    Version bump

--- client/defines.h
+++ client/defines.h
@@ -4,7 +4,7 @@
 #define APPLICATION_NAME    "AmneziaVPN"
 #define SERVICE_NAME        "AmneziaVPN-service"
 #define ORGANIZATION_NAME   "AmneziaVPN.ORG"
-#define APP_MAJOR_VERSION   "2.1.0"
-#define APP_VERSION         "2.1.0.0"
+#define APP_MAJOR_VERSION   "2.1.2"
+#define APP_VERSION         "2.1.2.0"
 
 #endif // DEFINES_H
