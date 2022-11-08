aea1d16e316eb347b546a179daf096c227f12436
vladimir.kuznetsov <astanaar1@gmail.com>
2022-11-08 21:32:17 +0300

    added files for linux and mac to CMakeLists for server and wireguard-service projects

--- service/src/qtservice.cmake
+++ service/src/qtservice.cmake
@@ -1,6 +1,6 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
+#include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
 
 if(NOT WIN32)
     set(LIBS ${LIBS} Qt6::Network)
@@ -16,6 +16,19 @@ set(HEADERS ${HEADERS}
 set(SOURCES ${SOURCES} 
     ${CMAKE_CURRENT_LIST_DIR}/qtservice.cpp
 )
+
+if(UNIX)
+    set(HEADERS ${HEADERS} 
+        ${CMAKE_CURRENT_LIST_DIR}/qtunixsocket.h
+        ${CMAKE_CURRENT_LIST_DIR}/qtunixserversocket.h
+    )
+
+    set(SOURCES ${SOURCES} 
+        ${CMAKE_CURRENT_LIST_DIR}/qtservice_unix.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/qtunixsocket.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/qtunixserversocket.cpp
+    )
+endif()
 
 if(WIN32)
     set(SOURCES ${SOURCES} 
