32cbb698ee7cc53371a6a06388d30ca3ac32167d
Dmitriy Karpushin <kolobchanin@gmail.com>
2023-01-16 14:30:08 +0300

    Support of apk signing for tag-deploy workflow

--- deploy/build_android.sh
+++ deploy/build_android.sh
@@ -20,12 +20,6 @@ APP_DOMAIN=org.amneziavpn.package
 OUT_APP_DIR=$BUILD_DIR/client
 BUNDLE_DIR=$OUT_APP_DIR/$APP_FILENAME
 
-INSTALLER_DATA_DIR=$BUILD_DIR/installer/packages/$APP_DOMAIN/data
-INSTALLER_BUNDLE_DIR=$BUILD_DIR/installer/$APP_FILENAME
-
-PRO_FILE_PATH=$PROJECT_DIR/$APP_NAME.pro
-QMAKE_STASH_FILE=$PROJECT_DIR/.qmake_stash
-
 # Seacrh Qt
 if [ -z "${QT_VERSION+x}" ]; then
 QT_VERSION=5.15.2;
@@ -35,10 +29,6 @@ fi
 echo "Using Qt in $QT_BIN_DIR"
 echo "Using Android SDK in $ANDROID_SDK_ROOT"
 echo "Using Android NDK in $ANDROID_NDK_ROOT"
-
-# Checking env
-# $QT_BIN_DIR/qmake -v
-# $ANDROID_NDK_HOME/prebuilt/linux-x86_64/bin/make -v
 
 # Build App
 echo "Building App..."
@@ -53,14 +43,7 @@ $QT_BIN_DIR/qt-cmake -S $PROJECT_DIR \
 
 cmake --build . --config release
 
-# $QT_BIN_DIR/qmake  -r -spec android-clang CONFIG+=qtquickcompiler ANDROID_ABIS="armeabi-v7a arm64-v8a x86 x86_64" $PROJECT_DIR/AmneziaVPN.pro
-# echo "Executing make... may take long time"
-# $ANDROID_NDK_HOME/prebuilt/linux-x86_64/bin/make -j2
-# echo "Make install..."
-# $ANDROID_NDK_HOME/prebuilt/linux-x86_64/bin/make install INSTALL_ROOT=android
-# echo "Build OK"
-
-echo "............Deploy.................."
+echo "............APK generation.................."
 cd $OUT_APP_DIR
 
 $QT_HOST_PATH/bin/androiddeployqt \
@@ -68,7 +51,7 @@ $QT_HOST_PATH/bin/androiddeployqt \
     --gradle \
     --release \
     --input android-AmneziaVPN-deployment-settings.json
-    
+   
 echo "............Copy apk.................."
 cp $OUT_APP_DIR/android-build/build/outputs/apk/release/android-build-release-unsigned.apk \
    $PROJECT_DIR/AmneziaVPN-release-unsigned.apk
