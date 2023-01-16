9f114a1dad1354c9b2d63cc7816fbc7d03cbdcc1
pokamest <pokamest@gmail.com>
2023-01-16 18:12:30 +0000

    Tiny fix [no ci]

--- client/server_scripts/install_docker.sh
+++ client/server_scripts/install_docker.sh
@@ -3,7 +3,7 @@ if [[ -f "$pm_apt" ]]; then pm=$pm_apt; docker_pkg="docker.io"; else pm=$pm_yum;
 if [[ ! -f "/usr/bin/sudo" ]]; then $pm update -y -q; $pm install -y -q sudo; fi;\
 docker_service=$(systemctl list-units --full -all | grep docker.service | grep -v inactive | grep -v dead | grep -v failed);\
 if [[ -f "$pm_apt" ]]; then export DEBIAN_FRONTEND=noninteractive; fi;\
-if [[ -z "$docker_service" ]]; then sudo  $pm update -y -q; sudo $pm install -y -q curl $docker_pkg; fi;\
+if [[ -z "$docker_service" ]]; then sudo $pm update -y -q; sudo $pm install -y -q curl $docker_pkg; fi;\
 docker_service=$(systemctl list-units --full -all | grep docker.service | grep -v inactive | grep -v dead | grep -v failed);\
 if [[ -z "$docker_service" ]]; then sleep 5 && sudo systemctl start docker && sleep 5; fi;\
 docker --version
