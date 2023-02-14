f0748568695f7246dae2973401449d90e766190b
Dmitriy Karpushin <kolobchanin@gmail.com>
2023-02-14 17:09:11 +0300

    Environment variables moved to cmake

--- client/android/cpp/cloak.cmake
+++ client/android/cpp/cloak.cmake
@@ -1,93 +1,45 @@
 set(TARGET ck_ovpn_plugin_go)
 
-list(APPEND CMAKE_PROGRAM_PATH "/usr/local/go/bin")
-message("*** pp - ${CMAKE_PROGRAM_PATH}")
-
-find_program(GO_EXEC go)
-
-message("*** fp - ${GO_EXEC}")
-
-message("*** toolchain ${CMAKE_TOOLCHAIN_FILE}")
-message("*** cc start $ENV{CC}")
-message("*** ndk ${CMAKE_ANDROID_NDK}")
-
-string(TOLOWER ${CMAKE_HOST_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR} HOST_TAG)
-message("*** HOST: ${HOST_TAG}")
-
-set(ANDROID_PREBUILT_TOOLCHAIN "${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/${HOST_TAG}")
-
-#set(MESSAGE_TST "TEST ")
-#set(ERROR_VER "ERR ")
-#message("*** ${MESSAGE_TST} ${ERROR_VER}")
-#execute_process(COMMAND pwd OUTPUT_VARIABLE MESSAGE_TST ERROR_VARIABLE ERROR_VER) # go version
-#message("*** after:")
-#message("*** ${MESSAGE_TST} ${ERROR_VER}")
-
 set(CLOAK_SRCS cloak/cmd/ck-ovpn-plugin/ck-ovpn-plugin.go)
 set(CLOAK_LIB ck-ovpn-plugin.so)
 
-set(ENV{GOPATH} "/Users/kolobchanin/go")
-set(ENV{CGO_ENABLED} 1)
-set(ENV{GOOS} "android")
+list(APPEND CMAKE_PROGRAM_PATH "/usr/local/go/bin")
+find_program(GO_EXEC go)
 
-message("*** os: $ENV{GOOS}")
-message("*** enabled: $ENV{CGO_ENABLED}")
-message("*** path: $ENV{GOPATH}")
-message("*** go flags: ${CMAKE_GO_FLAGS}")
+string(TOLOWER ${CMAKE_HOST_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR} HOST_TAG)
+set(ANDROID_PREBUILT_TOOLCHAIN "${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/${HOST_TAG}")
 
-set(MIN_API 24)
+# MIN_API must be exclusively a number
+set(MIN_API ${ANDROID_PLATFORM})
+
+set(BUILD_CMD_ARGS build)
+list(APPEND BUILD_CMD_ARGS -buildmode=c-shared -o ${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB} ${CMAKE_GO_FLAGS} ./...)
+
+set(PREPARE_ENV_ARGS env)
+list(APPEND PREPARE_ENV_ARGS -w CGO_ENABLED=1 GOOS=android)
 
 if ("${ANDROID_ABI}" STREQUAL "x86")
-	set(ENV{CC} "${ANDROID_PREBUILT_TOOLCHAIN}/bin/i686-linux-android${MIN_API}-clang")
-	set(ENV{GOARCH} "386")
-
-	add_custom_command(
-			OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}
-			DEPENDS ${CLOAK_SRCS}
-			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/cloak/cmd/ck-ovpn-plugin
-			COMMAND ${GO_EXEC} build -buildmode=c-shared -o "${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}" ${CMAKE_GO_FLAGS} ./...
-			COMMENT "Building Go library")
-
+	list(APPEND PREPARE_ENV_ARGS GOARCH=386)
+	list(APPEND PREPARE_ENV_ARGS CC=${ANDROID_PREBUILT_TOOLCHAIN}/bin/i686-linux-android${MIN_API}-clang)
 elseif ("${ANDROID_ABI}" STREQUAL "x86_64")
-	set(ENV{CC} "${ANDROID_PREBUILT_TOOLCHAIN}/bin/x86_64-linux-android${MIN_API}-clang")
-	set(ENV{GOARCH} "amd64")
-
-	add_custom_command(
-			OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}
-			DEPENDS ${CLOAK_SRCS}
-			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/cloak/cmd/ck-ovpn-plugin
-			COMMAND ${GO_EXEC} build -buildmode=c-shared -o "${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}" ${CMAKE_GO_FLAGS} ./...
-			COMMENT "Building Go library")
-
+	list(APPEND PREPARE_ENV_ARGS GOARCH=amd64)
+	list(APPEND PREPARE_ENV_ARGS CC=${ANDROID_PREBUILT_TOOLCHAIN}/bin/x86_64-linux-android${MIN_API}-clang)
 elseif ("${ANDROID_ABI}" STREQUAL "arm64-v8a")
-	set(ENV{CC} "${ANDROID_PREBUILT_TOOLCHAIN}/bin/aarch64-linux-android${MIN_API}-clang")
-	set(ENV{GOARCH} "arm64")
-
-	message("*** cc: $ENV{CC}")
-	message("*** arch: $ENV{GOARCH}")
-
-	add_custom_command(
-			OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}
-			DEPENDS ${CLOAK_SRCS}
-			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/cloak/cmd/ck-ovpn-plugin
-			COMMAND ${GO_EXEC} build -buildmode=c-shared -o "${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}" -ldflags=\"-s -w -v\" ./...
-			> /Users/kolobchanin/log.txt
-			COMMENT "Building Go library"
-			USES_TERMINAL)
-
+	list(APPEND PREPARE_ENV_ARGS GOARCH=arm64)
+	list(APPEND PREPARE_ENV_ARGS CC=${ANDROID_PREBUILT_TOOLCHAIN}/bin/aarch64-linux-android${MIN_API}-clang)
 elseif ("${ANDROID_ABI}" STREQUAL "armeabi-v7a")
-	set(ENV{CC} "${ANDROID_PREBUILT_TOOLCHAIN}/bin/armv7a-linux-androideabi${MIN_API}-clang")
-	set(ENV{GOARCH} "arm")
-	set(ENV{GOARM} "7")
-
-	add_custom_command(
-			OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}
-			DEPENDS ${CLOAK_SRCS}
-			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/cloak/cmd/ck-ovpn-plugin
-			COMMAND ${GO_EXEC} build -buildmode=c-shared -o "${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}" ${CMAKE_GO_FLAGS} ./...
-			COMMENT "Building Go library")
+	list(APPEND PREPARE_ENV_ARGS GOARCH=arm)
+	list(APPEND PREPARE_ENV_ARGS GOARM=7)
+	list(APPEND PREPARE_ENV_ARGS CC=${ANDROID_PREBUILT_TOOLCHAIN}/bin/armv7a-linux-androideabi${MIN_API}-clang)
 endif ()
 
+add_custom_command(
+		OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${CLOAK_LIB}
+		DEPENDS ${CLOAK_SRCS}
+		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/cloak/cmd/ck-ovpn-plugin
+		COMMAND ${GO_EXEC} ${PREPARE_ENV_ARGS}
+		COMMAND ${GO_EXEC} ${BUILD_CMD_ARGS}
+		COMMENT "Building Go library")
 
 add_custom_target(${TARGET} DEPENDS ${CLOAK_LIB} ${HEADER})
 add_library(ck-ovpn-plugin STATIC IMPORTED GLOBAL)
