671ef2a0f248a81a7605d41a6ecd4447f24476db
vladimir.kuznetsov <nethiuswork@gmail.com>
2023-03-24 13:51:40 +0300

    changed folder for generated files to CMAKE_BINARY_DIR

--- deploy/installer/config.cmake
+++ deploy/installer/config.cmake
@@ -2,22 +2,22 @@ if(WIN32)
     set(RootDir "@RootDir@")
     configure_file(
         ${CMAKE_CURRENT_LIST_DIR}/config/windows.xml.in
-        ${CMAKE_CURRENT_LIST_DIR}/config/windows.xml
+        ${CMAKE_BINARY_DIR}/installer/config/windows.xml
     )
 elseif(APPLE AND NOT IOS)
     configure_file(
         ${CMAKE_CURRENT_LIST_DIR}/config/macos.xml.in
-        ${CMAKE_CURRENT_LIST_DIR}/config/macos.xml
+        ${CMAKE_BINARY_DIR}/installer/config/macos.xml
     )
 elseif(LINUX)
     set(ApplicationsDir "@ApplicationsDir@")
     configure_file(
         ${CMAKE_CURRENT_LIST_DIR}/config/linux.xml.in
-        ${CMAKE_CURRENT_LIST_DIR}/config/linux.xml
+        ${CMAKE_BINARY_DIR}/installer/config/linux.xml
     )
 endif()
 
 configure_file(
     ${CMAKE_CURRENT_LIST_DIR}/packages/org.amneziavpn.package/meta/package.xml.in
-    ${CMAKE_CURRENT_LIST_DIR}/packages/org.amneziavpn.package/meta/package.xml
+    ${CMAKE_BINARY_DIR}/installer/packages/org.amneziavpn.package/meta/package.xml
 )
