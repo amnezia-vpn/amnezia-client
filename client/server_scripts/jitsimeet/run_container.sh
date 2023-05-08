84f64411cdd27e15e6a035f6a637f49ba6658570
Evgenii Muravev <evgenymuraviev@gmail.com>
2023-05-08 15:49:03 +0200

    Fixing typos

--- client/server_scripts/jitsimeet/run_container.sh
+++ client/server_scripts/jitsimeet/run_container.sh
@@ -1,8 +1,8 @@
 # Run container
-sudo docker network create meet.jitsi && docker run \
+sudo docker network create meet.jitsi && sudo docker run \
  -p 443:443 \
  -p 80:80 \
  -p 5222:5222 \
- --hostname xmpp.jitsi.meet \
+ --hostname xmpp.meet.jitsi \
  --network meet.jitsi \
  --name $CONTAINER_NAME $CONTAINER_NAME
