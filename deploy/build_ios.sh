c5f95750ccf4bf8505b41dda8c9af8765494b85c
pokamest <pokamest@gmail.com>
2023-05-09 13:15:53 -0700

    GH iOS build v2

--- deploy/build_ios.sh
+++ deploy/build_ios.sh
@@ -5,12 +5,11 @@ set -o errexit -o nounset
 
 # Hold on to current directory
 PROJECT_DIR=$(pwd)
-DEPLOY_DIR=$PROJECT_DIR/deploy
 
-mkdir -p $DEPLOY_DIR/build
-BUILD_DIR=$DEPLOY_DIR/build
+BUILD_DIR=$PROJECT_DIR/client/build-ios
+mkdir -p $BUILD_DIR
 
-echo "Project dir: ${PROJECT_DIR}" 
+echo "Project dir: ${PROJECT_DIR}"
 echo "Build dir: ${BUILD_DIR}"
 
 APP_NAME=AmneziaVPN
@@ -18,32 +17,56 @@ APP_FILENAME=$APP_NAME.app
 APP_DOMAIN=org.amneziavpn.package
 PLIST_NAME=$APP_NAME.plist
 
-OUT_APP_DIR=$BUILD_DIR/client
-BUNDLE_DIR=$OUT_APP_DIR/$APP_FILENAME
-
-PRO_FILE_PATH=$PROJECT_DIR/$APP_NAME.pro
-QMAKE_STASH_FILE=$PROJECT_DIR/.qmake_stash
 
 # Search Qt
 if [ -z "${QT_VERSION+x}" ]; then
-QT_VERSION=5.15.2;
+QT_VERSION=6.4.1;
 QIF_VERSION=4.1
 QT_BIN_DIR=$HOME/Qt/$QT_VERSION/ios/bin
 fi
 
 echo "Using Qt in $QT_BIN_DIR"
 
-
 # Checking env
-$QT_BIN_DIR/qmake -v
+$QT_BIN_DIR/qt-cmake --version
+cmake --version
+clang -v
 
-# Build App
-echo "Building App..."
-cd $PROJECT_DIR/client
-$PROJECT_DIR/client/scripts/apple_compile.sh ios
-
-# Build and run tests here
-
-#echo "............Deploy.................."
+# Generate XCodeProj
+#$QT_BIN_DIR/qt-cmake ./client -B $BUILD_DIR -GXcode
 
 
+# Setup keychain
+if [ "${IOS_DIST_SIGNING_KEY+x}" ]; then
+  echo "Import certificate"
+  echo $IOS_DIST_SIGNING_KEY | base64 --decode > $BUILD_DIR/signing-cert.p12
+
+  CERTIFICATE_P12=$BUILD_DIR/signing-cert.p12
+  KEYCHAIN=amnezia.build.keychain
+  TEMP_PASS=$IOS_DIST_SIGNING_KEY_PASSWORD
+
+  security create-keychain -p $TEMP_PASS $KEYCHAIN || true
+  security default-keychain -s $KEYCHAIN
+  security unlock-keychain -p $TEMP_PASS $KEYCHAIN
+
+  security default-keychain
+  security list-keychains
+
+  security import $CERTIFICATE_P12 -k $KEYCHAIN -P $IOS_DIST_SIGNING_KEY_PASSWORD -T /usr/bin/codesign || true
+
+  security set-key-partition-list -S "apple-tool:, apple:, codesign:" -s -k $TEMP_PASS $KEYCHAIN
+  security find-identity -p codesigning
+  security unlock-keychain -p $TEMP_PASS $KEYCHAIN
+
+fi
+
+# Build project
+xcodebuild \
+"OTHER_CODE_SIGN_FLAGS=--keychain '$HOME/Library/Keychains/amnezia.build.keychain-db'" \
+-configuration Release \
+-scheme AmneziaVPN \
+-destination "generic/platform=iOS,name=Any iOS'" \
+-project $BUILD_DIR/AmneziaVPN.xcodeproj
+
+# restore keychain
+security default-keychain -s login.keychain
