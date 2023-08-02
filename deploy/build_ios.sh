7740300c6066851a3df5dcfb233b772f4e20da19
pokamest <pokamest@gmail.com>
2023-08-01 18:23:09 -0700

    GA iOS buils fix

--- deploy/build_ios.sh
+++ deploy/build_ios.sh
@@ -41,14 +41,14 @@ KEYCHAIN_FILE=$HOME/Library/Keychains/${KEYCHAIN}-db
 if [ "${IOS_SIGNING_CERT_BASE64+x}" ]; then
   echo "Import certificate"
 
-  TRUST_CERT_P12=$BUILD_DIR/trust-cert.p12
+  TRUST_CERT_CER=$BUILD_DIR/trust-cert.cer
   SIGNING_CERT_P12=$BUILD_DIR/signing-cert.p12
 
-  echo $IOS_TRUST_CERT_BASE64 | base64 --decode > $BUILD_DIR/trust-cert.p12
-  echo $IOS_SIGNING_CERT_BASE64 | base64 --decode > $BUILD_DIR/signing-cert.p12
+  echo $IOS_TRUST_CERT_BASE64 | base64 --decode > $TRUST_CERT_CER
+  echo $IOS_SIGNING_CERT_BASE64 | base64 --decode > $SIGNING_CERT_P12
 
-  sha256sum $TRUST_CERT_P12
-  sha256sum $SIGNING_CERT_P12
+  shasum -a 256 $TRUST_CERT_CER
+  shasum -a 256 $SIGNING_CERT_P12
 
   KEYCHAIN_PASS=$IOS_SIGNING_CERT_PASSWORD
 
@@ -59,7 +59,7 @@ if [ "${IOS_SIGNING_CERT_BASE64+x}" ]; then
   security default-keychain
   security list-keychains
 
-  security import $TRUST_CERT_P12 -k $KEYCHAIN -P "" -T /usr/bin/codesign
+  security import $TRUST_CERT_CER -k $KEYCHAIN -P "" -T /usr/bin/codesign
   security import $SIGNING_CERT_P12 -k $KEYCHAIN -P $IOS_SIGNING_CERT_PASSWORD -T /usr/bin/codesign
 
   security set-key-partition-list -S "apple-tool:,apple:,codesign:" -s -k $KEYCHAIN_PASS $KEYCHAIN
@@ -74,8 +74,8 @@ if [ "${IOS_SIGNING_CERT_BASE64+x}" ]; then
   echo $IOS_APP_PROVISIONING_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/app.mobileprovision
   echo $IOS_NE_PROVISIONING_PROFILE | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/ne.mobileprovision
 
-  sha256sum ~/Library/MobileDevice/Provisioning\ Profiles/app.mobileprovision
-  sha256sum ~/Library/MobileDevice/Provisioning\ Profiles/ne.mobileprovision
+  shasum -a 256 ~/Library/MobileDevice/Provisioning\ Profiles/app.mobileprovision
+  shasum -a 256 ~/Library/MobileDevice/Provisioning\ Profiles/ne.mobileprovision
 
   profile_uuid=`grep UUID -A1 -a ~/Library/MobileDevice/Provisioning\ Profiles/app.mobileprovision | grep -io "[-A-F0-9]\{36\}"`
   profile_ne_uuid=`grep UUID -A1 -a ~/Library/MobileDevice/Provisioning\ Profiles/ne.mobileprovision | grep -io "[-A-F0-9]\{36\}"`
