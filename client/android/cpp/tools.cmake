f9ffcf4d15875f1e8889d44324ff555ed9fcc6a2
Mykola Baibuz <mykola.baibuz@gmail.com>
2023-08-03 17:44:58 -0400

    Move Android cpp code to openvpn-pt-android repo

--- client/android/cpp/tools.cmake
+++
@@ -1,7 +0,0 @@
-FUNCTION(PREPEND var prefix)
-   SET(listVar "")
-   FOREACH(f ${ARGN})
-      LIST(APPEND listVar "${prefix}/${f}")
-   ENDFOREACH(f)
-   SET(${var} "${listVar}" PARENT_SCOPE)
-ENDFUNCTION(PREPEND)
