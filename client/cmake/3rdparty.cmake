520f9ef4cba1f8bb26684c11697bb88a7908b87e
Mykola Baibuz <mykola.baibuz@gmail.com>
2023-08-07 18:03:06 -0400

    Change library linkage order (GNU feature)

--- client/cmake/3rdparty.cmake
+++ client/cmake/3rdparty.cmake
@@ -66,16 +66,16 @@ elseif(APPLE AND NOT IOS)
           DESTINATION ${OPENSSL_LIBRARIES_DIR})
 
   set(OPENSSL_USE_STATIC_LIBS TRUE)
-  set(LIBS ${LIBS}
-      ${OPENSSL_LIB_SSL_PATH}
-      ${OPENSSL_LIB_CRYPTO_PATH}
-  )
   
   set(LIBS ${LIBS} 
       ${LIBSSH_LIB_PATH} 
       ${ZLIB_LIB_PATH}
   )
-    
+  
+  set(LIBS ${LIBS}
+      ${OPENSSL_LIB_SSL_PATH}
+      ${OPENSSL_LIB_CRYPTO_PATH}
+  )
 
 add_compile_definitions(_WINSOCKAPI_)
 
