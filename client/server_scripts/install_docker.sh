4c690dd3c6d74acbfd97e643702456c991952da2
pokamest <pokamest@gmail.com>
2023-08-13 03:10:41 -0700

    Install scripts fixes (#275)
    * Install scripts fixes

--- client/server_scripts/install_docker.sh
+++ client/server_scripts/install_docker.sh
@@ -1,10 +1,20 @@
-pm_apt="/usr/bin/apt-get"; pm_yum="/usr/bin/yum";\
-if [[ -f "$pm_apt" ]]; then pm=$pm_apt; docker_pkg="docker.io"; else pm=$pm_yum; docker_pkg="docker"; fi;\
-if [[ ! -f "/usr/bin/sudo" ]]; then $pm update -y -q; $pm install -y -q sudo; fi;\
-docker_service=$(systemctl list-units --full -all | grep docker.service | grep -v inactive | grep -v dead | grep -v failed);\
-if [[ -f "$pm_apt" ]]; then export DEBIAN_FRONTEND=noninteractive; fi;\
-if [[ -z "$docker_service" ]]; then sudo $pm update -y -q; sudo $pm install -y -q curl $docker_pkg; fi;\
-docker_service=$(systemctl list-units --full -all | grep docker.service | grep -v inactive | grep -v dead | grep -v failed);\
-if [[ -z "$docker_service" ]]; then sleep 5 && sudo systemctl start docker && sleep 5; fi;\
-if [[ -f "$pm_yum" ]]; then sudo systemctl enable docker && sudo systemctl start docker; fi;\
+if which apt-get > /dev/null 2>&1; then pm=$(which apt-get); docker_pkg="docker.io"; dist="debian";\
+elif which dnf > /dev/null 2>&1; then pm=$(which dnf); docker_pkg="docker"; dist="fedora";\
+elif which yum > /dev/null 2>&1; then pm=$(which yum); docker_pkg="docker"; dist="centos";\
+else echo "Packet manager not found"; exit 1; fi;\
+echo "Dist: $dist, Packet manager: $pm, Docker pkg: $docker_pkg";\
+if [ "$dist" = "debian" ]; then export DEBIAN_FRONTEND=noninteractive; fi;\
+if ! command -v sudo > /dev/null 2>&1; then $pm update -yq; $pm install -yq sudo; fi;\
+if ! command -v fuser > /dev/null 2>&1; then $pm install -yq psmisc; fi;\
+if ! command -v lsof > /dev/null 2>&1; then $pm install -yq lsof; fi;\
+if ! command -v docker > /dev/null 2>&1; then $pm update -yq; $pm install -yq $docker_pkg;\
+  if [ "$dist" = "fedora" ] || [ "$dist" = "debian" ]; then sudo systemctl enable docker && sudo systemctl start docker; fi;\
+fi;\
+if [ "$dist" = "debian" ]; then \
+  docker_service=$(systemctl list-units --full --all | grep docker.service | grep -v inactive | grep -v dead | grep -v failed);\
+  if [ -z "$docker_service" ]; then sudo $pm update -yq; sudo $pm install -yq curl $docker_pkg; fi;\
+  sleep 3 && sudo systemctl start docker && sleep 3;\
+fi;\
+if ! command -v sudo > /dev/null 2>&1; then echo "Failed to install Docker";exit 1;fi;\
 docker --version
+
