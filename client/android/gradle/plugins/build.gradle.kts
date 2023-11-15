d77f3ecee899bcc9e75d0f92b1e5013c6b466713
albexk <albexk@proton.me>
2023-11-15 15:27:56 +0300

    Fix build error

--- client/android/gradle/plugins/build.gradle.kts
+++ client/android/gradle/plugins/build.gradle.kts
@@ -23,11 +23,3 @@ gradlePlugin {
         }
     }
 }
-
-// stop Gradle running by androiddeployqt
-gradle.taskGraph.whenReady {
-    if (providers.environmentVariable("ANDROIDDEPLOYQT_RUN").isPresent
-        && !providers.systemProperty("explicitRun").isPresent) {
-        tasks.forEach { it.enabled = false }
-    }
-}
