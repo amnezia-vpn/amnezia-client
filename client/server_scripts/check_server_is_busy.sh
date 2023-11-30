a8b56017779a8c69b81d32b84b05f27a2f03a4d6
lunardunno <126363523+lunardunno@users.noreply.github.com>
2023-11-30 15:24:39 +0400

    rollback check_server_is_busy.sh

--- client/server_scripts/check_server_is_busy.sh
+++ client/server_scripts/check_server_is_busy.sh
@@ -2,6 +2,4 @@ if which apt-get > /dev/null 2>&1; then LOCK_FILE="/var/lib/dpkg/lock-frontend";
 elif which dnf > /dev/null 2>&1; then LOCK_FILE="/var/run/dnf.pid";\
 elif which yum > /dev/null 2>&1; then LOCK_FILE="/var/run/yum.pid";\
 else echo "Packet manager not found"; echo "Internal error"; exit 1; fi;\
-if command -v fuser > /dev/null 2>&1; then \
-  if command -v sudo > /dev/null 2>&1; then sudo fuser $LOCK_FILE 2>/dev/null; else  echo "sudo not installed"; fi;\
-else  echo "fuser not installed"; fi
+if command -v fuser > /dev/null 2>&1; then sudo fuser $LOCK_FILE 2>/dev/null; else echo "fuser not installed"; fi
