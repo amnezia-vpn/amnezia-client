669f5c21dd5bc6b82054ad51ed1483639fd0a2d3
lunardunno <126363523+lunardunno@users.noreply.github.com>
2023-12-01 00:27:13 +0400

    small edit

--- client/server_scripts/check_server_is_busy.sh
+++ client/server_scripts/check_server_is_busy.sh
@@ -3,4 +3,4 @@ elif which dnf > /dev/null 2>&1; then LOCK_FILE="/var/run/dnf.pid";\
 elif which yum > /dev/null 2>&1; then LOCK_FILE="/var/run/yum.pid";\
 elif which pacman > /dev/null 2>&1; then LOCK_FILE="/var/lib/pacman/db.lck";\
 else echo "Packet manager not found"; echo "Internal error"; exit 1; fi;\
-if command -v fuser > /dev/null 2>&1; then sudo fuser $LOCK_FILE 2>/dev/null; else  echo "fuser not installed"; fi
+if command -v fuser > /dev/null 2>&1; then sudo fuser $LOCK_FILE 2>/dev/null; else echo "fuser not installed"; fi
