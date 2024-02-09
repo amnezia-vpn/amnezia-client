dba05aab07d0b5d1fbdc7da70e761bbf250276ed
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-02-09 17:00:44 +0400

    Improved docker cleanup. (#568)
    Improved docker cleanup.
    Removing anonymous volumes associated with containers. 
    Specifying the full name of the amn0 network interface to remove when cleaning the server.

--- client/server_scripts/remove_all_containers.sh
+++ client/server_scripts/remove_all_containers.sh
@@ -1,3 +1,4 @@
 sudo docker ps -a | grep amnezia | awk '{print $1}' | xargs sudo docker stop
-sudo docker ps -a | grep amnezia | awk '{print $1}' | xargs sudo docker rm
+sudo docker ps -a | grep amnezia | awk '{print $1}' | xargs sudo docker rm -fv
 sudo docker images -a | grep amnezia | awk '{print $3}' | xargs sudo docker rmi
+sudo docker network ls | grep amnezia-dns-net | awk '{print $1}' | xargs sudo docker network rm
