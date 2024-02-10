f3a168fd43a6a01b19e345db6b7e2536c735b331
isamnezia <156459471+isamnezia@users.noreply.github.com>
2024-02-11 01:55:54 +0300

    Codacy compatibility (#575)
     Refactor and split iostunnel with cmake changes, code cleanup

--- client/platforms/ios/Log.swift
+++ client/platforms/ios/Log.swift
@@ -13,59 +13,59 @@ struct Log {
   }
 
   private static let appGroupID = "group.org.amnezia.AmneziaVPN"
-  
+
   static let neLogURL = {
     let sharedContainerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupID)!
     return sharedContainerURL.appendingPathComponent("ne.log", isDirectory: false)
   }()
-  
+
   private static var sharedUserDefaults = {
     UserDefaults(suiteName: appGroupID)!
   }()
-  
+
   static let dateFormatter: DateFormatter = {
-      let dateFormatter = DateFormatter()
-      dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-      return dateFormatter
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    return dateFormatter
   }()
 
   var records: [Record]
-  
+
   init() {
     self.records = []
   }
-  
+
   init(_ str: String) {
     self.records = str.split(whereSeparator: \.isNewline)
       .compactMap {
         Record(String($0))!
       }
   }
-  
+
   init?(at url: URL) {
     if !FileManager.default.fileExists(atPath: url.path) {
-      guard let _ = try? "".data(using: .utf8)?.write(to: url) else { return nil }
+      guard (try? "".data(using: .utf8)?.write(to: url)) != nil else { return nil }
     }
-    
+
     guard let fileHandle = try? FileHandle(forUpdating: url) else { return nil }
-    
+
     defer { fileHandle.closeFile() }
-    
+
     guard
       let data = try? fileHandle.readToEnd(),
       let str = String(data: data, encoding: .utf8) else {
       return nil
     }
-    
+
     self.init(str)
   }
-  
+
   static func clear(at url: URL) {
     if FileManager.default.fileExists(atPath: url.path) {
       guard let fileHandle = try? FileHandle(forUpdating: url) else { return }
-      
+
       defer { fileHandle.closeFile() }
-      
+
       try? fileHandle.truncate(atOffset: 0)
     }
   }
--- client/platforms/ios/LogRecord.swift
+++ client/platforms/ios/LogRecord.swift
@@ -6,41 +6,41 @@ extension Log {
     let date: Date
     let level: Level
     let message: String
-    
+
     init?(_ str: String) {
       let dateStr = String(str.prefix(19))
       guard let date = Log.dateFormatter.date(from: dateStr) else { return nil }
-      
+
       let str = str.dropFirst(20)
-      
+
       guard let endIndex = str.firstIndex(of: " ") else { return nil }
       let levelStr = String(str[str.startIndex..<endIndex])
       guard let level = Level(rawValue: levelStr) else { return nil }
-      
+
       let messageStartIndex = str.index(after: endIndex)
       let message = String(str[messageStartIndex..<str.endIndex])
-      
+
       self.init(date: date, level: level, message: message)
     }
-    
+
     init(date: Date, level: Level, message: String) {
       self.date = date
       self.level = level
       self.message = message
     }
-    
+
     func save(at url: URL) {
       guard let data = "\n\(description)".data(using: .utf8) else { return }
-              
+
       if !FileManager.default.fileExists(atPath: url.path) {
-        guard let _ = try? "".data(using: .utf8)?.write(to: url) else { return }
+        guard (try? "".data(using: .utf8)?.write(to: url)) != nil else { return }
       }
-      
+
       guard let fileHandle = try? FileHandle(forUpdating: url) else { return }
-      
+
       defer { fileHandle.closeFile() }
-      
-      guard let _ = try? fileHandle.seekToEnd() else { return }
+
+      guard (try? fileHandle.seekToEnd()) != nil else { return }
       try? fileHandle.write(contentsOf: data)
     }
   }
@@ -61,7 +61,7 @@ extension Log.Record {
     case fatal
     case info
     case system // critical
-    
+
     init(from osLogType: OSLogType) {
       switch osLogType {
       case OSLogType.default:
