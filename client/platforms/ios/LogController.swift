097f84d938d61c9d503e8d489b1045cd21907be8
Igor Sorokin <is@amnezia.org>
2024-02-10 18:11:38 +0300

    Codacy compatibility (2)

--- client/platforms/ios/LogController.swift
+++ client/platforms/ios/LogController.swift
@@ -3,12 +3,12 @@ import Foundation
 public func swiftUpdateLogData(_ qtString: std.string) -> std.string {
   let qtLog = Log(String(describing: qtString))
   var log = qtLog
-
+  
   if let neLog = Log(at: Log.neLogURL) {
     neLog.records.forEach {
       log.records.append($0)
     }
-
+    
     log.records.sort {
       $0.date < $1.date
     }
