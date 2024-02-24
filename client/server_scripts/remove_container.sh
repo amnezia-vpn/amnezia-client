4d29306e2cdb2f98efb39d4c49f9c23d5d595e68
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-02-24 21:58:08 +0400

    rollback directory deletion

--- client/server_scripts/remove_container.sh
+++ client/server_scripts/remove_container.sh
@@ -1,4 +1,3 @@
 sudo docker stop $CONTAINER_NAME;\
 sudo docker rm -fv $CONTAINER_NAME;\
-sudo docker rmi $CONTAINER_NAME;\
-sudo rm -frd /opt/amnezia/$CONTAINER_NAME; sudo rmdir /opt/amnezia 2>/dev/null
+sudo docker rmi $CONTAINER_NAME
