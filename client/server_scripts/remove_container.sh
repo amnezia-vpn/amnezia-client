da75ada7bd0c12dac453b9cd78277b245dafab3d
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-02-24 17:56:30 +0400

    deleting container directory in opt
    Deleting the container directory in opt when deleting the container

--- client/server_scripts/remove_container.sh
+++ client/server_scripts/remove_container.sh
@@ -1,3 +1,4 @@
-sudo docker stop $CONTAINER_NAME
-sudo docker rm -fv $CONTAINER_NAME
-sudo docker rmi $CONTAINER_NAME
+sudo docker stop $CONTAINER_NAME;\
+sudo docker rm -fv $CONTAINER_NAME;\
+sudo docker rmi $CONTAINER_NAME;\
+sudo rm -frd /opt/amnezia/$CONTAINER_NAME; sudo rmdir /opt/amnezia 2>/dev/null
