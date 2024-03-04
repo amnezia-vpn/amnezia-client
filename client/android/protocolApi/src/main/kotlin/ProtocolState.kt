080e1d98c66ca4a22b376302b606dd5269a5cfbb
albexk <albexk@proton.me>
2024-03-04 18:08:55 +0300

    Add Quick Settings tile (#660)
    * Add Quick Settings tile
    
    - Add multi-client support to AmneziaVpnService
    - Make AmneziaActivity permanently connected to AmneziaVpnService while it is running
    - Refactor processing of connection state changes on qt side
    - Add VpnState DataStore
    - Add check if AmneziaVpnService is running
    
    * Add tile reset when the server is removed from the application

--- client/android/protocolApi/src/main/kotlin/ProtocolState.kt
+++ client/android/protocolApi/src/main/kotlin/ProtocolState.kt
@@ -2,9 +2,9 @@ package org.amnezia.vpn.protocol
 
 // keep synchronized with client/platforms/android/android_controller.h ConnectionState
 enum class ProtocolState {
+    DISCONNECTED,
     CONNECTED,
     CONNECTING,
-    DISCONNECTED,
     DISCONNECTING,
     RECONNECTING,
     UNKNOWN
--- client/android/src/org/amnezia/vpn/IpcMessenger.kt
+++ client/android/src/org/amnezia/vpn/IpcMessenger.kt
@@ -9,11 +9,21 @@ import org.amnezia.vpn.util.Log
 private const val TAG = "IpcMessenger"
 
 class IpcMessenger(
+    messengerName: String? = null,
     private val onDeadObjectException: () -> Unit = {},
-    private val onRemoteException: () -> Unit = {},
-    private val messengerName: String = "Unknown"
+    private val onRemoteException: () -> Unit = {}
 ) {
     private var messenger: Messenger? = null
+    val name = messengerName ?: "Unknown"
+
+    constructor(
+        messenger: Messenger,
+        messengerName: String? = null,
+        onDeadObjectException: () -> Unit = {},
+        onRemoteException: () -> Unit = {}
+    ) : this(messengerName, onDeadObjectException, onRemoteException) {
+        this.messenger = messenger
+    }
 
     fun set(messenger: Messenger) {
         this.messenger = messenger
@@ -25,19 +35,29 @@ class IpcMessenger(
 
     fun send(msg: () -> Message) = messenger?.sendMsg(msg())
 
+    fun send(msg: Message, replyTo: Messenger) = messenger?.sendMsg(msg.apply { this.replyTo = replyTo })
+
     fun <T> send(msg: T)
         where T : Enum<T>, T : IpcMessage = messenger?.sendMsg(msg.packToMessage())
+
+    fun <T> send(msg: T, replyTo: Messenger)
+        where T : Enum<T>, T : IpcMessage = messenger?.sendMsg(msg.packToMessage().apply { this.replyTo = replyTo })
 
     private fun Messenger.sendMsg(msg: Message) {
         try {
             send(msg)
         } catch (e: DeadObjectException) {
-            Log.w(TAG, "$messengerName messenger is dead")
+            Log.w(TAG, "$name messenger is dead")
             messenger = null
             onDeadObjectException()
         } catch (e: RemoteException) {
-            Log.w(TAG, "Sending a message to the $messengerName messenger failed: ${e.message}")
+            Log.w(TAG, "Sending a message to the $name messenger failed: ${e.message}")
             onRemoteException()
         }
     }
 }
+
+fun Map<Messenger, IpcMessenger>.send(msg: () -> Message) = this.values.forEach { it.send(msg) }
+
+fun <T> Map<Messenger, IpcMessenger>.send(msg: T)
+    where T : Enum<T>, T : IpcMessage = this.values.forEach { it.send(msg) }
