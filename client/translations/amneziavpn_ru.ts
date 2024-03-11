a2fc8cc88be32fe49e3bcd11cac143ef965456c1
Andrey Zaharow <andr290606@gmail.com>
2024-03-12 00:15:05 +0100

    Add RU translation for Error 1101 text

--- client/translations/amneziavpn_ru.ts
+++ client/translations/amneziavpn_ru.ts
@@ -2674,7 +2674,7 @@ and will not be shared or disclosed to the Amnezia or any third parties</source>
     <message>
         <location filename="../core/errorstrings.cpp" line="57"/>
         <source>This config has already been added to the application</source>
-        <translation type="unfinished"></translation>
+        <translation>Этот конфиг уже добавлен в приложение</translation>
     </message>
     <message>
         <location filename="../core/errorstrings.cpp" line="72"/>
