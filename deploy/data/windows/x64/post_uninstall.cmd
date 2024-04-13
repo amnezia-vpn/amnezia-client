dae589357cd004b5384f0f22c905980e6533896d
Mykola Baibuz <mykola.baibuz@gmail.com>
2024-04-13 23:41:20 +0300

    Close Amnezia application with service

--- deploy/data/windows/x64/post_uninstall.cmd
+++ deploy/data/windows/x64/post_uninstall.cmd
@@ -8,4 +8,5 @@ sc delete AmneziaVPN-service
 sc stop WireGuardTunnel$AmneziaVPN
 sc delete WireGuardTunnel$AmneziaVPN
 taskkill /IM "AmneziaVPN-service.exe" /F
+taskkill /IM "AmneziaVPN.exe" /F
 exit /b 0
