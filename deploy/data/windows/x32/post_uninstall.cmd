6a1e3c07b186453edba5325b0569e7bf10ab6691
albexk <albexk@proton.me>
2024-05-20 19:46:05 +0300

    Update AWG (v0.2.8) (#809)
    * Fix udpgso
    
    * Fix amneziawg run dir
    
    * Update Windows AWG binaries
    
    * Update AWG (v0.2.8)
    
    * Fix Windows pipe name
    
    * Fix Windows tunnel service name
    
    * Update Windows x86 AWG binary
    
    * Change default MTU for WireGuard and AWG
    
    * Fix preprocessor macros

--- deploy/data/windows/x32/post_uninstall.cmd
+++ deploy/data/windows/x32/post_uninstall.cmd
@@ -5,8 +5,8 @@ echo %AmneziaPath%
 timeout /t 1
 sc stop AmneziaVPN-service
 sc delete AmneziaVPN-service
-sc stop WireGuardTunnel$AmneziaVPN
-sc delete WireGuardTunnel$AmneziaVPN
+sc stop AmneziaWGTunnel$AmneziaVPN
+sc delete AmneziaWGTunnel$AmneziaVPN
 taskkill /IM "AmneziaVPN-service.exe" /F
 taskkill /IM "AmneziaVPN.exe" /F
 exit /b 0
