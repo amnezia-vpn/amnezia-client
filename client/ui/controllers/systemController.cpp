23b590de5073e21b068e9e29236c3fc85ffca420
Nethius <nethiuswork@gmail.com>
2024-05-22 14:02:11 +0700

    deleted unnecessary comment

--- client/ui/controllers/systemController.cpp
+++ client/ui/controllers/systemController.cpp
@@ -54,7 +54,6 @@ void SystemController::saveFile(QString fileName, const QString &data)
     QFileInfo fi(fileName);
 
 #ifdef Q_OS_MAC
-    // need for macOS but maybe will work on windows and linux too, need check
     const auto url = "file://" + fi.absoluteDir().absolutePath();
 #else
     const auto url = fi.absoluteDir().absolutePath();
