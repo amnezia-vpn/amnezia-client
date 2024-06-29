d502f1c4d2cdd130c1e0b27d00c81a056e0072c2
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-06-29 05:31:01 +0400

    correction of comment used

--- client/server_scripts/build_container.sh
+++ client/server_scripts/build_container.sh
@@ -8,7 +8,7 @@ if [ -n "$(docker --version 2>/dev/null | grep 'podman')" ]; then \
     sudo echo '[aliases]' >> $AliDir/$AliFile;\
     sudo chmod 600 $AliDir/$AliFile;\
   fi;\
-  if ! sudo grep -q '  # AmneziaVPN' $AliDir/$AliFile; then sudo echo -e \
+  if ! sudo grep -q '  # Amnezia start' $AliDir/$AliFile; then sudo echo -e \
     '  # Amnezia start\n  "3proxy/3proxy" = "docker.io/3proxy/3proxy"\n  "amneziavpn/amnezia-wg" = "docker.io/amneziavpn/amnezia-wg"\n  "amneziavpn/amneziawg-go" = "docker.io/amneziavpn/amneziawg-go"\n  "amneziavpn/ipsec-server" = "docker.io/amneziavpn/ipsec-server"\n  "amneziavpn/torpress" = "docker.io/amneziavpn/torpress"\n  "atmoz/sftp" = "docker.io/atmoz/sftp"\n  "mvance/unbound" = "docker.io/mvance/unbound"\n  "alpine" = "docker.io/library/alpine"\n  # Amnezia finish' \
     >> $AliDir/$AliFile;\
   fi;\
