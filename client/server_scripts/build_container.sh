520094d43034b97c1904bbbe1f1d30f6b13a6d69
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-06-30 12:45:40 +0400

    aliases reworked
    reworked the creation of aliases for podman for users with sudo

--- client/server_scripts/build_container.sh
+++ client/server_scripts/build_container.sh
@@ -1,16 +1,7 @@
 if [ -n "$(docker --version 2>/dev/null | grep 'podman')" ]; then \
-  AliDir=/var/cache/containers;\
-  AliFile=short-name-aliases.conf;\
-  if [ ! -d "$AliDir" ]; then \
-    sudo mkdir -m 700 -p $AliDir;\
-  fi;\
-  if [ ! -f "$AliDir/$AliFile" ]; then \
-    sudo echo '[aliases]' >> $AliDir/$AliFile;\
-    sudo chmod 600 $AliDir/$AliFile;\
-  fi;\
-  if ! sudo grep -q '  # Amnezia start' $AliDir/$AliFile; then sudo echo -e \
-    '  # Amnezia start\n  "3proxy/3proxy" = "docker.io/3proxy/3proxy"\n  "amneziavpn/amnezia-wg" = "docker.io/amneziavpn/amnezia-wg"\n  "amneziavpn/amneziawg-go" = "docker.io/amneziavpn/amneziawg-go"\n  "amneziavpn/ipsec-server" = "docker.io/amneziavpn/ipsec-server"\n  "amneziavpn/torpress" = "docker.io/amneziavpn/torpress"\n  "atmoz/sftp" = "docker.io/atmoz/sftp"\n  "mvance/unbound" = "docker.io/mvance/unbound"\n  "alpine" = "docker.io/library/alpine"\n  # Amnezia finish' \
-    >> $AliDir/$AliFile;\
-  fi;\
+  sudo sh -c "if [ ! -d '/var/cache/containers' ]; then mkdir -m 700 -p /var/cache/containers; fi";\
+  sudo sh -c "if [ ! -f '/var/cache/containers/short-name-aliases.conf' ]; then touch /var/cache/containers/short-name-aliases.conf; chmod 600 /var/cache/containers/short-name-aliases.conf; fi";\
+  sudo sh -c "if ! grep -q '[aliases]' /var/cache/containers/short-name-aliases.conf; then echo '[aliases]' >> /var/cache/containers/short-name-aliases.conf; fi";\
+  sudo sh -c "if ! grep -q '  # Amnezia start' /var/cache/containers/short-name-aliases.conf; then echo '  # Amnezia start\n  \"3proxy/3proxy\" = \"docker.io/3proxy/3proxy\"\n  \"amneziavpn/amnezia-wg\" = \"docker.io/amneziavpn/amnezia-wg\"\n  \"amneziavpn/amneziawg-go\" = \"docker.io/amneziavpn/amneziawg-go\"\n  \"amneziavpn/ipsec-server\" = \"docker.io/amneziavpn/ipsec-server\"\n  \"amneziavpn/torpress\" = \"docker.io/amneziavpn/torpress\"\n  \"atmoz/sftp\" = \"docker.io/atmoz/sftp\"\n  \"mvance/unbound\" = \"docker.io/mvance/unbound\"\n  \"alpine\" = \"docker.io/library/alpine\"\n  # Amnezia finish' >> /var/cache/containers/short-name-aliases.conf; fi";\
 fi;\
 sudo docker build --no-cache --pull -t $CONTAINER_NAME $DOCKERFILE_FOLDER
