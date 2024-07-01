5c9ab510cbcbd2c080ffeb3a0d21fdf5d4f09110
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-07-01 15:13:58 +0400

    tests removed

--- client/server_scripts/remove_all_containers.sh
+++ client/server_scripts/remove_all_containers.sh
@@ -5,17 +5,4 @@ sudo docker network ls | grep amnezia-dns-net | awk '{print $1}' | xargs sudo do
 sudo rm -frd /opt/amnezia;\
 if [ -n "$(sudo docker --version 2>/dev/null | grep 'podman')" ]; then \
   sudo sed -i '/^  # Amnezia start/,/^  # Amnezia finish$/d' /var/cache/containers/short-name-aliases.conf;\
-fi;\
-sudo cat >> /var/cache/containers/short-name-aliases.conf << EOF
-# Amnezia start
-"3proxy/3proxy" = "docker.io/3proxy/3proxy"
-"amneziavpn/amnezia-wg" = "docker.io/amneziavpn/amnezia-wg"
-"amneziavpn/amneziawg-go" = "docker.io/amneziavpn/amneziawg-go"
-"amneziavpn/ipsec-server" = "docker.io/amneziavpn/ipsec-server"
-"amneziavpn/torpress" = "docker.io/amneziavpn/torpress"
-"atmoz/sftp" = "docker.io/atmoz/sftp"
-"mvance/unbound" = "docker.io/mvance/unbound"
-"alpine" = "docker.io/library/alpine"
-# Amnezia finish
-EOF
 fi
