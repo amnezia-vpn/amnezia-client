37cddd73ceb647428b6be8601d734b4cca1cfd90
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-07-17 21:16:55 +0400

    Using grep exit code

--- client/server_scripts/remove_all_containers.sh
+++ client/server_scripts/remove_all_containers.sh
@@ -3,6 +3,4 @@ sudo docker ps -a | grep amnezia | awk '{print $1}' | xargs sudo docker rm -fv;\
 sudo docker images -a | grep amnezia | awk '{print $3}' | xargs sudo docker rmi;\
 sudo docker network ls | grep amnezia-dns-net | awk '{print $1}' | xargs sudo docker network rm;\
 sudo rm -frd /opt/amnezia;\
-if [ -n "$(sudo docker --version 2>/dev/null | grep 'podman')" ]; then \
-  sudo sed -i '/^  # Amnezia start/,/^  # Amnezia finish$/d' /var/cache/containers/short-name-aliases.conf;\
-fi
+sudo docker --version 2>/dev/null | grep -q 'podman' && sudo sed -i '/^  # Amnezia start/,/^  # Amnezia finish$/d' /var/cache/containers/short-name-aliases.conf
