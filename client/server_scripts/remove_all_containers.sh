d1b3c05e024c8a0a31af16cf7330450531f0bd85
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-07-18 04:26:38 +0400

    Removing quotes

--- client/server_scripts/remove_all_containers.sh
+++ client/server_scripts/remove_all_containers.sh
@@ -3,4 +3,5 @@ sudo docker ps -a | grep amnezia | awk '{print $1}' | xargs sudo docker rm -fv;\
 sudo docker images -a | grep amnezia | awk '{print $3}' | xargs sudo docker rmi;\
 sudo docker network ls | grep amnezia-dns-net | awk '{print $1}' | xargs sudo docker network rm;\
 sudo rm -frd /opt/amnezia;\
-sudo docker --version 2>/dev/null | grep -q 'podman' && sudo sed -i '/^  # Amnezia start/,/^  # Amnezia finish$/d' /var/cache/containers/short-name-aliases.conf
+sudo docker --version 2>/dev/null | grep -q podman && \
+  sudo sed -i '/^  # Amnezia start/,/^  # Amnezia finish$/d' /var/cache/containers/short-name-aliases.conf
