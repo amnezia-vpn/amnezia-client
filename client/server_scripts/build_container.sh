df743b0b39fe2fe52579729754d57e238d786f6a
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-07-18 18:56:17 +0400

    Improved code readability

--- client/server_scripts/build_container.sh
+++ client/server_scripts/build_container.sh
@@ -1,23 +1,18 @@
-if [ -n "$(sudo docker --version 2>/dev/null | grep podman)" ]; then \
-  sudo test -d "/var/cache/containers" || \
-    sudo mkdir -m 700 -p /var/cache/containers;\
-  sudo test ! -f "/var/cache/containers/short-name-aliases.conf" && \
-    sudo touch /var/cache/containers/short-name-aliases.conf && \
-    sudo chmod 600 /var/cache/containers/short-name-aliases.conf;\
-  sudo grep -q "\[aliases\]" /var/cache/containers/short-name-aliases.conf || \
-    sudo sh -c "echo '[aliases]' >> /var/cache/containers/short-name-aliases.conf";\
-  sudo grep -q "  # Amnezia start" /var/cache/containers/short-name-aliases.conf || \
-    sudo sh -c "printf '%s\n' \
-      '  # Amnezia start' \
-      '  \"3proxy/3proxy\" = \"docker.io/3proxy/3proxy\"' \
-      '  \"amneziavpn/amnezia-wg\" = \"docker.io/amneziavpn/amnezia-wg\"' \
-      '  \"amneziavpn/amneziawg-go\" = \"docker.io/amneziavpn/amneziawg-go\"' \
-      '  \"amneziavpn/ipsec-server\" = \"docker.io/amneziavpn/ipsec-server\"' \
-      '  \"amneziavpn/torpress\" = \"docker.io/amneziavpn/torpress\"' \
-      '  \"atmoz/sftp\" = \"docker.io/atmoz/sftp\"' \
-      '  \"mvance/unbound\" = \"docker.io/mvance/unbound\"' \
-      '  \"alpine\" = \"docker.io/library/alpine\"' \
-      '  # Amnezia finish' \
+if [ -n "$(sudo docker --version 2>/dev/null | grep podman)" ]; then sudo sh -c "\
+  test -d /var/cache/containers || mkdir -m 700 -p /var/cache/containers;\
+  test -f /var/cache/containers/short-name-aliases.conf || chmod 600 /var/cache/containers/short-name-aliases.conf>>/var/cache/containers/short-name-aliases.conf;\
+  grep -q '\[aliases\]' /var/cache/containers/short-name-aliases.conf || echo '[aliases]' >> /var/cache/containers/short-name-aliases.conf;\
+  grep -q '  # Amnezia start' /var/cache/containers/short-name-aliases.conf || printf '%s\n' \
+    '  # Amnezia start' \
+    '  \"3proxy/3proxy\" = \"docker.io/3proxy/3proxy\"' \
+    '  \"amneziavpn/amnezia-wg\" = \"docker.io/amneziavpn/amnezia-wg\"' \
+    '  \"amneziavpn/amneziawg-go\" = \"docker.io/amneziavpn/amneziawg-go\"' \
+    '  \"amneziavpn/ipsec-server\" = \"docker.io/amneziavpn/ipsec-server\"' \
+    '  \"amneziavpn/torpress\" = \"docker.io/amneziavpn/torpress\"' \
+    '  \"atmoz/sftp\" = \"docker.io/atmoz/sftp\"' \
+    '  \"mvance/unbound\" = \"docker.io/mvance/unbound\"' \
+    '  \"alpine\" = \"docker.io/library/alpine\"' \
+    '  # Amnezia finish' \
     >> /var/cache/containers/short-name-aliases.conf";\
 fi;\
 sudo docker build --no-cache --pull -t $CONTAINER_NAME $DOCKERFILE_FOLDER
