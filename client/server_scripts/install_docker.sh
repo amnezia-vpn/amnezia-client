ef902e6ddb4ab3c58b4e3d49d308075f03afa296
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-07-19 17:18:55 +0400

    Changes 2

--- client/server_scripts/install_docker.sh
+++ client/server_scripts/install_docker.sh
@@ -14,16 +14,15 @@ if ! command -v sudo > /dev/null 2>&1; then $pm $check_pkgs; $pm $silent_inst su
 if ! command -v fuser > /dev/null 2>&1; then sudo $pm $check_pkgs; sudo $pm $silent_inst psmisc || fuser 2>&1 > /dev/null || exit 1; fi;\
 if ! command -v lsof > /dev/null 2>&1; then sudo $pm $check_pkgs; sudo $pm $silent_inst lsof || lsof 2>&1 > /dev/null || exit 1; fi;\
 if ! command -v docker > /dev/null 2>&1; then sudo $pm $check_pkgs;\
-  if [ -n "$($pm $wh_pkg $docker_pkg 2>/dev/null | grep moby-engine)" ]; then echo "Docker is not supported"; docker; exit 1;\
-  else sudo $pm $silent_inst $docker_pkg;\
-    if ! command -v docker > /dev/null 2>&1; then docker; exit 1;\
-    elif [ -n "$(sudo docker --version 2>&1 | grep 'podman')" ]; then check_srv="podman.socket podman"; sudo touch /etc/containers/nodocker; fi;\
-  sleep 5; sudo systemctl enable --now $check_srv; sleep 5;\
+  if [ -n "$($pm $wh_pkg $docker_pkg 2>/dev/null | grep moby-engine)" ]; then echo "Docker is not supported"; echo "command not found"; exit 1;\
+  else sudo $pm $silent_inst $docker_pkg || docker 2>&1 > /dev/null || exit 1;\
   fi;\
+  if [ -n "$(sudo docker --version 2>/dev/null | grep podman)" ]; then check_srv="podman.socket podman"; sudo touch /etc/containers/nodocker; fi;\
+  sleep 5; sudo systemctl enable --now $check_srv; sleep 5;\
 fi;\
-if [ -n "$(sudo docker --version 2>&1 | grep 'moby-engine')" ]; then echo "Docker is not supported"; echo "command not found"; exit 1;\
-elif [ -n "$(sudo docker --version 2>&1 | grep 'podman')" ]; then check_srv="podman.socket podman"; docker_pkg="podman-docker";\
-  if [ -n "$(sudo docker --version 2>&1 | grep '/etc/containers/nodocker')" ]; then sudo touch /etc/containers/nodocker; fi;\
+if [ -n "$(sudo docker --version 2>&1 | grep moby-engine)" ]; then echo "Docker is not supported"; echo "command not found"; exit 1;\
+elif [ -n "$(sudo docker --version 2>&1 | grep podman)" ]; then check_srv="podman.socket podman"; docker_pkg="podman-docker";\
+  if [ -n "$(sudo docker --version 2>&1 | grep /etc/containers/nodocker)" ]; then sudo touch /etc/containers/nodocker; fi;\
 fi;\
 if [ "$(systemctl is-active $check_srv | head -n1)" != "active" ]; then \
   sudo $pm $check_pkgs; sudo $pm $silent_inst $docker_pkg;\
