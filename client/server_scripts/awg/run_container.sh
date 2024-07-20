284019f954e8eaf4242be11540e43531c10e7699
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-07-20 07:10:41 +0400

    Create service for podman

--- client/server_scripts/awg/run_container.sh
+++ client/server_scripts/awg/run_container.sh
@@ -11,8 +11,12 @@ sudo docker run -d \
 --name $CONTAINER_NAME \
 $CONTAINER_NAME
 
+# Create service for podman
+sudo sh -c 'docker --version 2>/dev/null | grep -q podman && \
+podman generate systemd --name $CONTAINER_NAME 2>/dev/null > /opt/amnezia/$CONTAINER_NAME/container-$CONTAINER_NAME.service && \
+systemctl enable /opt/amnezia/$CONTAINER_NAME/container-$CONTAINER_NAME.service > /dev/null'
+
 sudo docker network connect amnezia-dns-net $CONTAINER_NAME
 
 # Prevent to route packets outside of the container in case if server behind of the NAT
 #sudo docker exec -i $CONTAINER_NAME sh -c "ifconfig eth0:0 $SERVER_IP_ADDRESS netmask 255.255.255.255 up"
-
