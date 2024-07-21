92e33f62c599d2746eda3f9182e07a1fa07d5cca
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-07-22 01:57:38 +0400

    Clearing server for podman

--- client/server_scripts/remove_all_containers.sh
+++ client/server_scripts/remove_all_containers.sh
@@ -1,7 +1,9 @@
 sudo docker ps -a | grep amnezia | awk '{print $1}' | xargs sudo docker stop;\
+sudo docker --version 2>/dev/null | grep -q podman && \
+  sudo systemctl list-units | grep amnezia | awk '{print $1}' | xargs sudo systemctl disable --now && \
+  sudo systemctl daemon-reload && sudo systemctl reset-failed && \
+  sudo sed -i '/^  # Amnezia start/,/^  # Amnezia finish$/d' /var/cache/containers/short-name-aliases.conf;\
 sudo docker ps -a | grep amnezia | awk '{print $1}' | xargs sudo docker rm -fv;\
 sudo docker images -a | grep amnezia | awk '{print $3}' | xargs sudo docker rmi;\
 sudo docker network ls | grep amnezia-dns-net | awk '{print $1}' | xargs sudo docker network rm;\
-sudo rm -frd /opt/amnezia;\
-sudo docker --version 2>/dev/null | grep -q podman && \
-  sudo sed -i '/^  # Amnezia start/,/^  # Amnezia finish$/d' /var/cache/containers/short-name-aliases.conf
+sudo rm -frd /opt/amnezia
