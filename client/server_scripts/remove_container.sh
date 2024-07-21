edee3fec108a4d8ea50f1531b88e386fd18d574a
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-07-22 02:12:52 +0400

    Removing service for podman

--- client/server_scripts/remove_container.sh
+++ client/server_scripts/remove_container.sh
@@ -1,4 +1,7 @@
-sudo systemctl disable --now container-$CONTAINER_NAME.service;\
 sudo docker stop $CONTAINER_NAME;\
-sudo docker rm -fv $CONTAINER_NAME && sudo rm -f $DOCKERFILE_FOLDER/container-$CONTAINER_NAME.service;\
+sudo docker --version 2>/dev/null | grep -q podman && \
+  sudo systemctl disable --now container-$CONTAINER_NAME.service && \
+  sudo systemctl daemon-reload && sudo systemctl reset-failed && \
+  sudo rm -f $DOCKERFILE_FOLDER/container-$CONTAINER_NAME.service;\
+sudo docker rm -fv $CONTAINER_NAME;\
 sudo docker rmi $CONTAINER_NAME
