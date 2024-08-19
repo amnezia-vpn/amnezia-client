6c5d590169a9f70dd9dc5f76f4d64b09218dad78
Nethius <nethiuswork@gmail.com>
2024-08-19 16:17:09 +0700

    fixed xray port processing (#983)
    * fixed xray port processing
    
    * fixed saving port when changing xray settings and saving transport protocol when changing all the protocols settings

--- client/ui/models/protocols/awgConfigModel.cpp
+++ client/ui/models/protocols/awgConfigModel.cpp
@@ -26,24 +26,12 @@ bool AwgConfigModel::setData(const QModelIndex &index, const QVariant &value, in
     case Roles::JunkPacketCountRole: m_protocolConfig.insert(config_key::junkPacketCount, value.toString()); break;
     case Roles::JunkPacketMinSizeRole: m_protocolConfig.insert(config_key::junkPacketMinSize, value.toString()); break;
     case Roles::JunkPacketMaxSizeRole: m_protocolConfig.insert(config_key::junkPacketMaxSize, value.toString()); break;
-    case Roles::InitPacketJunkSizeRole:
-        m_protocolConfig.insert(config_key::initPacketJunkSize, value.toString());
-        break;
-    case Roles::ResponsePacketJunkSizeRole:
-        m_protocolConfig.insert(config_key::responsePacketJunkSize, value.toString());
-        break;
-    case Roles::InitPacketMagicHeaderRole:
-        m_protocolConfig.insert(config_key::initPacketMagicHeader, value.toString());
-        break;
-    case Roles::ResponsePacketMagicHeaderRole:
-        m_protocolConfig.insert(config_key::responsePacketMagicHeader, value.toString());
-        break;
-    case Roles::UnderloadPacketMagicHeaderRole:
-        m_protocolConfig.insert(config_key::underloadPacketMagicHeader, value.toString());
-        break;
-    case Roles::TransportPacketMagicHeaderRole:
-        m_protocolConfig.insert(config_key::transportPacketMagicHeader, value.toString());
-        break;
+    case Roles::InitPacketJunkSizeRole: m_protocolConfig.insert(config_key::initPacketJunkSize, value.toString()); break;
+    case Roles::ResponsePacketJunkSizeRole: m_protocolConfig.insert(config_key::responsePacketJunkSize, value.toString()); break;
+    case Roles::InitPacketMagicHeaderRole: m_protocolConfig.insert(config_key::initPacketMagicHeader, value.toString()); break;
+    case Roles::ResponsePacketMagicHeaderRole: m_protocolConfig.insert(config_key::responsePacketMagicHeader, value.toString()); break;
+    case Roles::UnderloadPacketMagicHeaderRole: m_protocolConfig.insert(config_key::underloadPacketMagicHeader, value.toString()); break;
+    case Roles::TransportPacketMagicHeaderRole: m_protocolConfig.insert(config_key::transportPacketMagicHeader, value.toString()); break;
     }
 
     emit dataChanged(index, index, QList { role });
@@ -82,6 +70,8 @@ void AwgConfigModel::updateModel(const QJsonObject &config)
 
     QJsonObject protocolConfig = config.value(config_key::awg).toObject();
 
+    auto defaultTransportProto = ProtocolProps::transportProtoToString(ProtocolProps::defaultTransportProto(Proto::Awg), Proto::Awg);
+    m_protocolConfig.insert(config_key::transport_proto, protocolConfig.value(config_key::transport_proto).toString(defaultTransportProto));
     m_protocolConfig[config_key::last_config] = protocolConfig.value(config_key::last_config);
     m_protocolConfig[config_key::port] = protocolConfig.value(config_key::port).toString(protocols::awg::defaultPort);
     m_protocolConfig[config_key::mtu] = protocolConfig.value(config_key::mtu).toString(protocols::awg::defaultMtu);
@@ -98,14 +88,11 @@ void AwgConfigModel::updateModel(const QJsonObject &config)
     m_protocolConfig[config_key::initPacketMagicHeader] =
             protocolConfig.value(config_key::initPacketMagicHeader).toString(protocols::awg::defaultInitPacketMagicHeader);
     m_protocolConfig[config_key::responsePacketMagicHeader] =
-            protocolConfig.value(config_key::responsePacketMagicHeader)
-                    .toString(protocols::awg::defaultResponsePacketMagicHeader);
+            protocolConfig.value(config_key::responsePacketMagicHeader).toString(protocols::awg::defaultResponsePacketMagicHeader);
     m_protocolConfig[config_key::underloadPacketMagicHeader] =
-            protocolConfig.value(config_key::underloadPacketMagicHeader)
-                    .toString(protocols::awg::defaultUnderloadPacketMagicHeader);
+            protocolConfig.value(config_key::underloadPacketMagicHeader).toString(protocols::awg::defaultUnderloadPacketMagicHeader);
     m_protocolConfig[config_key::transportPacketMagicHeader] =
-            protocolConfig.value(config_key::transportPacketMagicHeader)
-                    .toString(protocols::awg::defaultTransportPacketMagicHeader);
+            protocolConfig.value(config_key::transportPacketMagicHeader).toString(protocols::awg::defaultTransportPacketMagicHeader);
 
     endResetModel();
 }
@@ -163,22 +150,17 @@ AwgConfig::AwgConfig(const QJsonObject &jsonConfig)
     port = jsonConfig.value(config_key::port).toString(protocols::awg::defaultPort);
     mtu = jsonConfig.value(config_key::mtu).toString(protocols::awg::defaultMtu);
     junkPacketCount = jsonConfig.value(config_key::junkPacketCount).toString(protocols::awg::defaultJunkPacketCount);
-    junkPacketMinSize =
-            jsonConfig.value(config_key::junkPacketMinSize).toString(protocols::awg::defaultJunkPacketMinSize);
-    junkPacketMaxSize =
-            jsonConfig.value(config_key::junkPacketMaxSize).toString(protocols::awg::defaultJunkPacketMaxSize);
-    initPacketJunkSize =
-            jsonConfig.value(config_key::initPacketJunkSize).toString(protocols::awg::defaultInitPacketJunkSize);
-    responsePacketJunkSize =
-            jsonConfig.value(config_key::responsePacketJunkSize).toString(protocols::awg::defaultResponsePacketJunkSize);
-    initPacketMagicHeader =
-            jsonConfig.value(config_key::initPacketMagicHeader).toString(protocols::awg::defaultInitPacketMagicHeader);
-    responsePacketMagicHeader = jsonConfig.value(config_key::responsePacketMagicHeader)
-                                        .toString(protocols::awg::defaultResponsePacketMagicHeader);
-    underloadPacketMagicHeader = jsonConfig.value(config_key::underloadPacketMagicHeader)
-                                         .toString(protocols::awg::defaultUnderloadPacketMagicHeader);
-    transportPacketMagicHeader = jsonConfig.value(config_key::transportPacketMagicHeader)
-                                         .toString(protocols::awg::defaultTransportPacketMagicHeader);
+    junkPacketMinSize = jsonConfig.value(config_key::junkPacketMinSize).toString(protocols::awg::defaultJunkPacketMinSize);
+    junkPacketMaxSize = jsonConfig.value(config_key::junkPacketMaxSize).toString(protocols::awg::defaultJunkPacketMaxSize);
+    initPacketJunkSize = jsonConfig.value(config_key::initPacketJunkSize).toString(protocols::awg::defaultInitPacketJunkSize);
+    responsePacketJunkSize = jsonConfig.value(config_key::responsePacketJunkSize).toString(protocols::awg::defaultResponsePacketJunkSize);
+    initPacketMagicHeader = jsonConfig.value(config_key::initPacketMagicHeader).toString(protocols::awg::defaultInitPacketMagicHeader);
+    responsePacketMagicHeader =
+            jsonConfig.value(config_key::responsePacketMagicHeader).toString(protocols::awg::defaultResponsePacketMagicHeader);
+    underloadPacketMagicHeader =
+            jsonConfig.value(config_key::underloadPacketMagicHeader).toString(protocols::awg::defaultUnderloadPacketMagicHeader);
+    transportPacketMagicHeader =
+            jsonConfig.value(config_key::transportPacketMagicHeader).toString(protocols::awg::defaultTransportPacketMagicHeader);
 }
 
 bool AwgConfig::hasEqualServerSettings(const AwgConfig &other) const
@@ -186,8 +168,7 @@ bool AwgConfig::hasEqualServerSettings(const AwgConfig &other) const
     if (port != other.port || junkPacketCount != other.junkPacketCount || junkPacketMinSize != other.junkPacketMinSize
         || junkPacketMaxSize != other.junkPacketMaxSize || initPacketJunkSize != other.initPacketJunkSize
         || responsePacketJunkSize != other.responsePacketJunkSize || initPacketMagicHeader != other.initPacketMagicHeader
-        || responsePacketMagicHeader != other.responsePacketMagicHeader
-        || underloadPacketMagicHeader != other.underloadPacketMagicHeader
+        || responsePacketMagicHeader != other.responsePacketMagicHeader || underloadPacketMagicHeader != other.underloadPacketMagicHeader
         || transportPacketMagicHeader != other.transportPacketMagicHeader) {
         return false;
     }
