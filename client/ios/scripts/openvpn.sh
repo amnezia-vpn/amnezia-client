b13311899194a004347e0c9a873e5b3c18482bb2
sobolevn <mail@sobolevn.me>
2024-08-28 14:29:09 +0300

    Fix `openvpn.sh` for `pwd` paths with spaces

--- client/ios/scripts/openvpn.sh
+++ client/ios/scripts/openvpn.sh
@@ -2,11 +2,11 @@ XCODEBUILD="/usr/bin/xcodebuild"
 WORKINGDIR=`pwd`
 PATCH="/usr/bin/patch"
 
-  cat $WORKINGDIR/3rd/OpenVPNAdapter/Configuration/Project.xcconfig > $WORKINGDIR/3rd/OpenVPNAdapter/Configuration/amnezia.xcconfig
-  cat << EOF >> $WORKINGDIR/3rd/OpenVPNAdapter/Configuration/amnezia.xcconfig
-  PROJECT_TEMP_DIR = $WORKINGDIR/3rd/OpenVPNAdapter/build/OpenVPNAdapter.build
-  CONFIGURATION_BUILD_DIR = $WORKINGDIR/3rd/OpenVPNAdapter/build/Release-iphoneos
-  BUILT_PRODUCTS_DIR = $WORKINGDIR/3rd/OpenVPNAdapter/build/Release-iphoneos
+  cat "$WORKINGDIR/3rd/OpenVPNAdapter/Configuration/Project.xcconfig" > "$WORKINGDIR/3rd/OpenVPNAdapter/Configuration/amnezia.xcconfig"
+  cat << EOF >> "$WORKINGDIR/3rd/OpenVPNAdapter/Configuration/amnezia.xcconfig"
+  PROJECT_TEMP_DIR = "$WORKINGDIR/3rd/OpenVPNAdapter/build/OpenVPNAdapter.build"
+  CONFIGURATION_BUILD_DIR = "$WORKINGDIR/3rd/OpenVPNAdapter/build/Release-iphoneos"
+  BUILT_PRODUCTS_DIR = "$WORKINGDIR/3rd/OpenVPNAdapter/build/Release-iphoneos"
 EOF
 
 
