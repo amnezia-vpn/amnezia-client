f6b8632562035ba0c8d6f5f8be24a0f3547478a5
albexk <albexk@proton.me>
2024-10-30 17:35:33 +0300

    Add methods to verify and purchase subscriptions

--- client/android/src/org/amnezia/vpn/AmneziaActivity.kt
+++ client/android/src/org/amnezia/vpn/AmneziaActivity.kt
@@ -63,7 +63,6 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import org.amnezia.vpn.protocol.getStatistics
 import org.amnezia.vpn.protocol.getStatus
 import org.amnezia.vpn.qt.QtAndroidController
@@ -1120,15 +1119,9 @@ class AmneziaActivity : QtActivity() {
     @Suppress("unused")
     fun getAppList(): String {
         Log.v(TAG, "Get app list")
-        var appList = ""
-        runBlocking {
-            mainScope.launch {
-                withContext(Dispatchers.IO) {
-                    appList = AppListProvider.getAppList(packageManager, packageName)
-                }
-            }.join()
+        return blockingCall(Dispatchers.IO) {
+            AppListProvider.getAppList(packageManager, packageName)
         }
-        return appList
     }
 
     @Suppress("unused")
@@ -1303,28 +1296,46 @@ class AmneziaActivity : QtActivity() {
     @Suppress("unused")
     fun getCountryCode(): String {
         Log.v(TAG, "Get country code")
-        return runBlocking {
-            mainScope.async {
-                billingRepository.getCountryCode()
-            }.await()
-        }
+        return blockingCall { billingRepository.getCountryCode() }
     }
 
     @Suppress("unused")
     fun getSubscriptionPlans(): String {
         Log.v(TAG, "Get subscription plans")
-        return runBlocking {
-            mainScope.async {
-                billingRepository.getSubscriptionPlans()
-            }.await()
+        return blockingCall { billingRepository.getSubscriptionPlans() }
+    }
+
+    @Suppress("unused")
+    fun purchaseSubscription(offerToken: String): String {
+        Log.v(TAG, "Purchase subscription")
+        return blockingCall { billingRepository.purchaseSubscription(this@AmneziaActivity, offerToken) }
+    }
+
+    @Suppress("unused")
+    fun upgradeSubscription(offerToken: String, oldPurchaseToken: String): String {
+        Log.v(TAG, "Upgrade subscription")
+        return blockingCall {
+            billingRepository.upgradeSubscription(this@AmneziaActivity, offerToken, oldPurchaseToken)
         }
+    }
+
+    @Suppress("unused")
+    fun acknowledgePurchase(purchaseToken: String): String {
+        Log.v(TAG, "Acknowledge purchase")
+        return blockingCall { billingRepository.acknowledge(purchaseToken) }
+    }
+
+    @Suppress("unused")
+    fun queryPurchases(): String {
+        Log.v(TAG, "Query purchases")
+        return blockingCall { billingRepository.queryPurchases() }
     }
 
     /**
      * Utils methods
      */
     private fun <T> blockingCall(
-        context: CoroutineContext = Dispatchers.Main.immediate,
+        context: CoroutineContext = Dispatchers.Default,
         block: suspend () -> T
     ) = runBlocking {
         mainScope.async(context) { block() }.await()
--- client/android/src/org/amnezia/vpn/BillingRepository.kt
+++ client/android/src/org/amnezia/vpn/BillingRepository.kt
@@ -5,5 +5,8 @@ import android.app.Activity
 interface BillingRepository {
     suspend fun getCountryCode(): String
     suspend fun getSubscriptionPlans(): String
-    suspend fun purchaseSubscription(activity: Activity): String
+    suspend fun purchaseSubscription(activity: Activity, offerToken: String): String
+    suspend fun upgradeSubscription(activity: Activity, offerToken: String, oldPurchaseToken: String): String
+    suspend fun acknowledge(purchaseToken: String): String
+    suspend fun queryPurchases(): String
 }
