69e18ed4f3cecc111d9e29cbcd1081bce46e7fca
lunardunno <126363523+lunardunno@users.noreply.github.com>
2025-03-03 11:54:28 +0400

    Moved removing timestamp sudo
    Removing the sudo timestamp is done every time.

--- client/server_scripts/check_user_in_sudo.sh
+++ client/server_scripts/check_user_in_sudo.sh
@@ -6,6 +6,7 @@ else pm="uname"; opt="-a";\
 fi;\
 CUR_USER=$(whoami 2>/dev/null || echo ~ | sed 's/.*\///');\
 echo $LANG | grep -qE '^(en_US.UTF-8|C.UTF-8|C)$' || export LC_ALL=C;\
+sudo -K;\
 if [ "$CUR_USER" = "root" ] || ( groups "$CUR_USER" | grep -E '\<(sudo|wheel)\>' ); then \
-  sudo -K; sudo -nu $CUR_USER $pm $opt > /dev/null; sudo -n $pm $opt > /dev/null;\
+  sudo -nu $CUR_USER $pm $opt > /dev/null; sudo -n $pm $opt > /dev/null;\
 fi
