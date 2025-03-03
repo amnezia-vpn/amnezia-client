0125688d8c35b4061778baaddf526369342d9aff
lunardunno <126363523+lunardunno@users.noreply.github.com>
2025-03-03 08:11:12 +0400

    Returned nested launch
    Returned nested launch via sudo

--- client/server_scripts/check_user_in_sudo.sh
+++ client/server_scripts/check_user_in_sudo.sh
@@ -7,5 +7,5 @@ fi;\
 CUR_USER=$(whoami 2>/dev/null || echo ~ | sed 's/.*\///');\
 echo $LANG | grep -qE '^(en_US.UTF-8|C.UTF-8|C)$' || export LC_ALL=C;\
 if [ "$CUR_USER" = "root" ] || ( groups "$CUR_USER" | grep -E '\<(sudo|wheel)\>' ); then \
-  sudo -K && sudo -n $pm $opt > /dev/null;\
+  sudo -K && sudo -nu $CUR_USER $pm $opt > /dev/null && sudo -n $pm $opt > /dev/null;\
 fi
