41b52e900ccd0caeb16ea56bdfeb6fd5f8661820
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2025-03-24 13:19:28 +0100

    feat: update app identifiers and development team for iOS and macOS builds

--- client/platforms/macos/macosutils.mm
+++ client/platforms/macos/macosutils.mm
@@ -21,7 +21,7 @@ NSString* MacOSUtils::appId() {
   if (!appId) {
     // Fallback. When an unsigned/un-notarized app is executed in
     // command-line mode, it could fail the fetching of its own bundle id.
-    appId = @"org.amnezia.AmneziaVPN";
+    appId = @"org.amneziaUnipessoal.DefaultVPN";
   }
 
   return appId;
