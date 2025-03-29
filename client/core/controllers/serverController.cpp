92296188afad9d157d120704f9812ef601afecf6
Peter Galonza <p.galonza@evaron.ru>
2025-03-29 22:59:12 +0300

    fix: set iptables rules after reboot

--- client/core/controllers/serverController.cpp
+++ client/core/controllers/serverController.cpp
@@ -417,8 +417,18 @@ ErrorCode ServerController::installDockerWorker(const ServerCredentials &credent
 
 ErrorCode ServerController::prepareHostWorker(const ServerCredentials &credentials, DockerContainer container, const QJsonObject &config)
 {
-    // create folder on host
-    return runScript(credentials, replaceVars(amnezia::scriptData(SharedScriptType::prepare_host), genVarsForScript(credentials, container)));
+    QString sctiptFileName = QString("/opt/amnezia/setup_host_firewall.sh")
+    errorCode = uploadFileToHost(credentials, amnezia::scriptData(SharedScriptType::setup_host_firewall).toUtf8(), sctiptFileName);
+
+    if (errorCode)
+        return errorCode;
+
+    ErrorCode errorCode = runScript(credentials, replaceVars(amnezia::scriptData(SharedScriptType::prepare_host), genVarsForScript(credentials, container)));
+
+    if (errorCode)
+        return errorCode;
+
+    return errorCode
 }
 
 ErrorCode ServerController::buildContainerWorker(const ServerCredentials &credentials, DockerContainer container, const QJsonObject &config)
