b342efe7a0af89f8e036200a3b5072ab0b0846f7
Mitternacht822 <sb@amnezia.org>
2025-05-15 17:25:34 +0400

    fixed issue when autostart option was not preserving after backup

--- client/ui/controllers/settingsController.cpp
+++ client/ui/controllers/settingsController.cpp
@@ -140,9 +140,28 @@ void SettingsController::restoreAppConfigFromData(const QByteArray &data)
 {
     bool ok = m_settings->restoreAppConfig(data);
     if (ok) {
+        QJsonObject newConfigData = QJsonDocument::fromJson(data).object();
+
+
+#if defined(Q_OS_WINDOWS) || defined(Q_OS_LINUX) || defined(Q_OS_MACX)
+        //bool autoStartEnabled = newConfigData.value("Conf/autoStart").toBool();
+        bool autoStartEnabled = m_settings->isAutoStart();
+
+        qDebug() << "Restored autoStartEnabled:" << autoStartEnabled;
+        qDebug() << "Settings value:" << m_settings->isAutoStart();
+
+        //m_settings->setAutoStart(autoStartEnabled);
+        toggleAutoStart(autoStartEnabled);
+
+        qDebug() << "Restored autoStartEnabled:" << autoStartEnabled;
+        qDebug() << "Settings value:" << m_settings->isAutoStart();
+        //m_settings->sync();
+#endif
+
         m_serversModel->resetModel();
         m_languageModel->changeLanguage(
                 static_cast<LanguageSettings::AvailableLanguageEnum>(m_languageModel->getCurrentLanguageIndex()));
+
         emit restoreBackupFinished();
     } else {
         emit changeSettingsErrorOccurred(tr("Backup file is corrupted"));
