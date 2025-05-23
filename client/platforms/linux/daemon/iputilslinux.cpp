c494a9e6e757706c50df7b0103c8fde68c6efecc
vladimir.kuznetsov <nethiuswork@gmail.com>
2025-05-23 14:18:42 +0800

    feature: extended logging in service part

--- client/platforms/linux/daemon/iputilslinux.cpp
+++ client/platforms/linux/daemon/iputilslinux.cpp
@@ -97,7 +97,7 @@ bool IPUtilsLinux::addIP4AddressToDevice(const InterfaceConfig& config) {
   // Set ifr to interface
   int ret = ioctl(sockfd, SIOCSIFADDR, &ifr);
   if (ret) {
-    logger.error() << "Failed to set IPv4: " << logger.sensitive(deviceAddr)
+    logger.error() << "Failed to set IPv4: " << deviceAddr
                    << "error:" << strerror(errno);
     return false;
   }
@@ -138,7 +138,7 @@ bool IPUtilsLinux::addIP6AddressToDevice(const InterfaceConfig& config) {
   // Set ifr6 to the interface
   ret = ioctl(sockfd, SIOCSIFADDR, &ifr6);
   if (ret && (errno != EEXIST)) {
-    logger.error() << "Failed to set IPv6: " << logger.sensitive(deviceAddr)
+    logger.error() << "Failed to set IPv6: " << deviceAddr
                    << "error:" << strerror(errno);
     return false;
   }
