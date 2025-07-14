343f876afd2202d0b8e0fc2e03d137615e275a74
MrMirDan <mirdan0@yandex.ru>
2025-07-14 18:57:43 +0300

    some changes

--- client/translations/amneziavpn_ru_RU.ts
+++ client/translations/amneziavpn_ru_RU.ts
@@ -1522,10 +1522,6 @@ Thank you for staying with us!</source>
         <translation>Поддержите Amnezia</translation>
     </message>
     <message>
-        <source>Show other methods on GithHub</source>
-        <translation type="vanished">Показать другие способы на GitHub</translation>
-    </message>
-    <message>
         <location filename="../ui/qml/Pages2/PageSettingsAbout.qml" line="159"/>
         <source>Amnezia is a free and open-source application. You can support the developers if you like it.</source>
         <translation>Amnezia — это бесплатное приложение с открытым исходным кодом. Поддержите разработчиков, если оно вам нравится.</translation>
@@ -2460,14 +2456,6 @@ Thank you for staying with us!</source>
         <translation>Исключения для DNS</translation>
     </message>
     <message>
-        <source>DNS servers listed here will be excluded from KillSwitch restrictions and remain accessible when KillSwitch is active.</source>
-        <translation type="vanished">Перечисленные DNS-серверы будут исключены из ограничений KillSwitch и останутся доступными при активном KillSwitch.</translation>
-    </message>
-    <message>
-        <source>DNS servers from the list will remain accessible when KillSwitch is triggered</source>
-        <translation type="vanished">DNS-серверы из этого списка будут исключены из ограничений KillSwitch и останутся доступными при активном KillSwitch.</translation>
-    </message>
-    <message>
         <location filename="../ui/qml/Pages2/PageSettingsKillSwitchExceptions.qml" line="46"/>
         <source>DNS servers listed here will remain accessible when KillSwitch is active</source>
         <translation>DNS-серверы из этого списка останутся доступными при активном KillSwitch</translation>
@@ -2768,10 +2756,6 @@ Thank you for staying with us!</source>
         <source>Management</source>
         <translation>Управление</translation>
     </message>
-    <message>
-        <source>Data</source>
-        <translation type="vanished">Данные</translation>
-    </message>
 </context>
 <context>
     <name>PageSettingsServerProtocol</name>
@@ -2862,10 +2846,6 @@ Thank you for staying with us!</source>
         <location filename="../ui/qml/Pages2/PageSettingsSplitTunneling.qml" line="33"/>
         <source>Default server does not support split tunneling function</source>
         <translation>Сервер по умолчанию не поддерживает раздельное туннелирование</translation>
-    </message>
-    <message>
-        <source>Only the sites listed here will be accessed via VPN</source>
-        <translation type="vanished">Только адреса из списка должны открываться через VPN</translation>
     </message>
     <message>
         <location filename="../ui/qml/Pages2/PageSettingsSplitTunneling.qml" line="71"/>
@@ -4184,7 +4164,7 @@ Thank you for staying with us!</source>
     <message>
         <location filename="../core/errorstrings.cpp" line="73"/>
         <source>Failed to decrypt response payload</source>
-        <translation></translation>
+        <translation>Не удалось расшифровать ответ полезной нагрузки</translation>
     </message>
     <message>
         <location filename="../core/errorstrings.cpp" line="74"/>
@@ -4195,10 +4175,6 @@ Thank you for staying with us!</source>
         <location filename="../core/errorstrings.cpp" line="75"/>
         <source>The limit of allowed configurations per subscription has been exceeded</source>
         <translation>Превышен лимит разрешенных конфигураций для одной подписки</translation>
-    </message>
-    <message>
-        <source>A migration has error occurred. Please contact our technical support</source>
-        <translation type="vanished">Произошла ошибка миграции. Обратитесь в нашу техническую поддержку</translation>
     </message>
     <message>
         <location filename="../core/errorstrings.cpp" line="81"/>
