6a63b6f70d7d6bb9a661270518dab0f1642a3be6
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2025-07-24 22:14:44 +0300

    fix: Log spamming during xray connection fixed

--- client/platforms/ios/ios_controller.mm
+++ client/platforms/ios/ios_controller.mm
@@ -253,17 +253,19 @@ void IosController::checkStatus()
         uint64_t rxBytes = [response[@"rx_bytes"] intValue];
         
         uint64_t last_handshake_time_sec = 0;
+#if !MACOS_NE
         if (response[@"last_handshake_time_sec"] && ![response[@"last_handshake_time_sec"] isKindOfClass:[NSNull class]]) {
             last_handshake_time_sec = [response[@"last_handshake_time_sec"] intValue];
         } else {
             qDebug() << "Key last_handshake_time_sec is missing or null";
         }
-        
+
         if (last_handshake_time_sec < 0) {
             disconnectVpn();
             qDebug() << "Invalid handshake time, disconnecting VPN.";
         }
-        
+#endif
+
         emit bytesChanged(rxBytes - m_rxBytes, txBytes - m_txBytes);
         m_rxBytes = rxBytes;
         m_txBytes = txBytes;
