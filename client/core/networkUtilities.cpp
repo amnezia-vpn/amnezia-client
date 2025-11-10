9894a3678112f37be32551ac663bb4758dd18487
VoyNaLunu <lalalalaskir@gmail.com>
2025-11-10 19:13:03 +0300

    revert some changes because fix turned out to be simpler

--- client/core/networkUtilities.cpp
+++ client/core/networkUtilities.cpp
@@ -170,11 +170,11 @@ int NetworkUtilities::AdapterIndexTo(const QHostAddress& dst) {
 #ifdef Q_OS_WIN
     qDebug() << "Getting Current Internet Adapter that routes to"
              << dst.toString();
+    quint32 ipBigEndian;
     quint32 ip = dst.toIPv4Address();
-    quint32 ipBigEndian = qToBigEndian(ip);
-    DWORD dwDestAddr = static_cast<DWORD>(ipBigEndian);
+    qToBigEndian(ip, &ipBigEndian);
     _MIB_IPFORWARDROW routeInfo;
-    auto result = GetBestRoute(dwDestAddr, 0, &routeInfo);
+    auto result = GetBestRoute(ipBigEndian, 0, &routeInfo);
     if (result != NO_ERROR) {
         return -1;
     }
