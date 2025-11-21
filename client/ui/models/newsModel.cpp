37fa106493a49478c79f46bdd13fc973c50eb6d7
MrMirDan <mirdan0@yandex.ru>
2025-11-21 14:50:41 +0200

    fix: update or delete news on newsModel update

--- client/ui/models/newsModel.cpp
+++ client/ui/models/newsModel.cpp
@@ -79,26 +79,50 @@ void NewsModel::setProcessedIndex(int index)
 void NewsModel::updateModel(const QJsonArray &serverItems)
 {
     QSet<QString> existingIds;
+
     for (const NewsItem &item : m_items) {
         existingIds.insert(item.id);
     }
 
-    QList<NewsItem> newItems;
+    QList<QJsonObject> objects;
+    QSet<QString> ids;
+
     for (const QJsonValue &value : serverItems) {
         if (!value.isObject())
             continue;
-        const QJsonObject obj = value.toObject();
-        QString id = obj.value("id").toString();
 
-        if (!existingIds.contains(id)) {
+        objects.append(value.toObject());
+        ids.insert(value.toObject().value("id").toString());
+    }
+
+    QList<NewsItem> newItems;
+
+    int i = 0;
+    while (!ids.contains(existingIds)) {
+        if (!ids.contains(m_items[i].id)) {
+            QString toRemove = m_items[i].id;
+            m_items.erase(m_items.begin() + i);
+            existingIds.remove(toRemove);
+        }
+        i++;
+    }
+
+    for (i = 0; i < objects.size(); i++) {
+        if (!existingIds.contains(objects[i].value("id").toString())) {
             NewsItem item;
-            item.id = id;
-            item.title = obj.value("title").toString();
-            item.content = obj.value("content").toString();
-            item.timestamp = QDateTime::fromString(obj.value("timestamp").toString(), Qt::ISODate);
-            item.read = m_readIds.contains(id);
+            item.id = objects[i].value("id").toString();
+            item.title = objects[i].value("title").toString();
+            item.content = objects[i].value("content").toString();
+            item.timestamp = QDateTime::fromString(objects[i].value("timestamp").toString(), Qt::ISODate);
+            item.read = m_readIds.contains(objects[i].value("id").toString());
             newItems.append(item);
-            existingIds.insert(id);
+            existingIds.insert(item.id);
+        } else {
+            if (m_items[i].timestamp != QDateTime::fromString(objects[i].value("timestamp").toString(), Qt::ISODate)) {
+                m_items[i].title = objects[i].value("title").toString();
+                m_items[i].content = objects[i].value("content").toString();
+                m_items[i].timestamp = QDateTime::fromString(objects[i].value("timestamp").toString(), Qt::ISODate);
+            }
         }
     }
 
