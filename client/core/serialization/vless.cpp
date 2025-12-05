01d7782040b8b0b0b4e8bc914f266515c4175d58
Vlad <247929764+curly-buzzard-ncnf@users.noreply.github.com>
2025-12-05 19:25:26 +0300

    Fix spiderX field name in VLESS and Trojan parsing

--- client/core/serialization/vless.cpp
+++ client/core/serialization/vless.cpp
@@ -232,9 +232,9 @@ QJsonObject Deserialize(const QString &str, QString *alias, QString *errMessage)
             const auto pbk = QUrl::fromPercentEncoding(query.queryItemValue("pbk").toUtf8());
             QJsonIO::SetValue(stream, pbk, { "realitySettings", "publicKey" });
         }
-        if (query.hasQueryItem("spiderX"))
+        if (query.hasQueryItem("spx"))
         {
-            const auto spiderX = QUrl::fromPercentEncoding(query.queryItemValue("spiderX").toUtf8());
+            const auto spiderX = QUrl::fromPercentEncoding(query.queryItemValue("spx").toUtf8());
             QJsonIO::SetValue(stream, spiderX, { "realitySettings", "spiderX" });
         }
         if (query.hasQueryItem("sid"))
