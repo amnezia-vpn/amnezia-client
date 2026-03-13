07c3d1206b08c69a8ba6c30c970b7e5dfb3b5a89
MrMirDan <mirdan0@yandex.ru>
2026-03-13 11:15:36 +0200

    update: native exports test

--- client/core/controllers/coreController.h
+++ client/core/controllers/coreController.h
@@ -88,6 +88,7 @@ class TestComplexOperations;
 class TestSettingsSignals;
 class TestUiServersModelAndController;
 class TestSelfHostedServerSetup;
+class TestMultipleExports;
 
 class CoreController : public QObject
 {
@@ -105,6 +106,7 @@ class CoreController : public QObject
     friend class TestSettingsSignals;
     friend class TestUiServersModelAndController;
     friend class TestSelfHostedServerSetup;
+    friend class TestMultipleExports;
 
 public:
     explicit CoreController(const QSharedPointer<VpnConnection> &vpnConnection, SecureQSettings* settings,
