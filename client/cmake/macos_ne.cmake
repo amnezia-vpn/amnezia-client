6623a4d7f7d40ca6dd3211f6c43892aeebb0f52d
nyKilka <reznikila23@gmail.com>
2026-03-23 18:52:45 +0300

    modified:   client/cmake/macos_ne.cmake

--- client/cmake/macos_ne.cmake
+++ client/cmake/macos_ne.cmake
@@ -122,7 +122,7 @@ target_compile_options(${PROJECT} PRIVATE
     -DVPN_NE_BUNDLEID=\"${BUILD_IOS_APP_IDENTIFIER}.network-extension\"
 )
 
-set(WG_APPLE_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rd/fblinkwg-apple/Sources)
+set(WG_APPLE_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rd/amneziawg-apple/Sources)
 
 target_sources(${PROJECT} PRIVATE
     ${WG_APPLE_SOURCE_DIR}/WireGuardKitC/x25519.c
