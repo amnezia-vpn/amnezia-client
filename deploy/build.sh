d81c1e5c1af99dd6378a35dc701e9c2a2ec7300d
Yaroslav Gurov <ygurov@proton.me>
2026-04-16 01:57:25 +0200

    feat: rework CI/CD for macos

---
+++ deploy/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit
+
+PROJECT_DIR=$(pwd)
+BUILD_DIR="$PROJECT_DIR/deploy/build"
+
+folders=()
+for base in ~/Qt /opt/Qt; do
+    for dir in "$base"/${QT_VERSION:-6.*}; do
+        [ -d "$dir" ] && folders+=("$dir")
+    done
+done
+
+: ${QT_ROOT_PATH:=$(printf '%s\n' "${folders[@]}" | sort -V | tail -1)}
+
+case "$(uname -s)" in
+    Linux)
+        : ${QT_PREFIX_PATH:="$QT_ROOT_PATH"/gcc_64}
+        ;;
+    Darwin)
+        : ${QT_PREFIX_PATH:="$QT_ROOT_PATH"/macos}
+        ;;
+esac
+
+args=()
+if [ -n "${QT_PREFIX_PATH}" ]; then
+    args+=("-DCMAKE_PREFIX_PATH=$QT_PREFIX_PATH")
+fi
+
+set -o xtrace
+
+cmake -S "$PROJECT_DIR" -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release "${args[@]}"
+cmake --build "$BUILD_DIR" --target all
+(cd "$BUILD_DIR" && cpack)
