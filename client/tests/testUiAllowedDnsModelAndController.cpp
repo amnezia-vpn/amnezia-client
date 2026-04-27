b280b2aacdaf0420b68c0032f883b6f1d408d871
MrMirDan <mirdan0@yandex.ru>
2026-04-27 15:13:20 +0300

    update: some qverify2 messages

--- client/tests/testUiAllowedDnsModelAndController.cpp
+++ client/tests/testUiAllowedDnsModelAndController.cpp
@@ -81,7 +81,8 @@ private slots:
         QVERIFY2(allowedDnsModelIndex.isValid(), "Site model index should be valid");
 
         auto dnsIp = m_coreController->m_allowedDnsModel->data(allowedDnsModelIndex, AllowedDnsModel::IpRole);
-        QVERIFY2(dnsIp == ip, QString("app path should be %1, got %2").arg(ip, appPath));
+        QString msg = QString("dns ip should be %1, got %2").arg(ip, dnsIp.toString());
+        QVERIFY2(dnsIp == ip, msg.toLocal8Bit().constData());
 
         m_coreController->m_allowedDnsUiController->importDns(getPath(), true);
         m_coreController->m_allowedDnsUiController->updateModel();
