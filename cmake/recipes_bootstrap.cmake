a5c5fa8c6da3e6301621c3dcb30a990fcaf8062a
Yaroslav Gurov <ygurov@proton.me>
2026-05-01 16:08:57 +0200

    feat: add hints for conan on MacOS

--- cmake/recipes_bootstrap.cmake
+++ cmake/recipes_bootstrap.cmake
@@ -1,4 +1,8 @@
-find_program(CONAN_COMMAND "conan" REQUIRED)
+find_program(CONAN_COMMAND "conan" REQUIRED
+    HINTS
+        "${CMAKE_SOURCE_DIR}/.venv/bin"
+        "/opt/homebrew/bin"
+)
 
 file(GLOB_RECURSE LOCAL_RECIPES "${CMAKE_SOURCE_DIR}/recipes/*/conanfile.py")
 foreach(RECIPE ${LOCAL_RECIPES})
