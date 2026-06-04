373fe0448190f306df9926627ca7df2b6cc16cee
NickVs2015 <nv@amnezia.org>
2026-06-04 12:00:19 +0300

    fix: add custom target for Play billing

--- client/cmake/android.cmake
+++ client/cmake/android.cmake
@@ -83,20 +83,43 @@ endif()
 
 set_property(TARGET ${PROJECT} PROPERTY QT_ANDROID_PACKAGE_SOURCE_DIR ${APP_ANDROID_PACKAGE_SOURCE_DIR})
 
+if(QT_USE_TARGET_ANDROID_BUILD_DIR)
+    set(_android_build_dir "${CMAKE_CURRENT_BINARY_DIR}/android-build-${PROJECT}")
+else()
+    set(_android_build_dir "${CMAKE_CURRENT_BINARY_DIR}/android-build")
+endif()
+
+add_custom_target(android_gradle_clean
+    COMMAND ./gradlew clean
+    WORKING_DIRECTORY "${_android_build_dir}"
+    COMMENT "Cleaning Android Gradle build cache"
+)
+
+# Always-available debug target: build Play Debug APK and copy to standard output path
+# so Qt Creator's deploy step picks it up automatically
+add_custom_target(android_play_debug_install
+    COMMAND ./gradlew assemblePlayDebug
+    COMMAND sh -c "cp build/outputs/apk/play/debug/*.apk build/outputs/apk/android-build-${PROJECT}-debug.apk"
+    WORKING_DIRECTORY "${_android_build_dir}"
+    COMMENT "Building Android Play Debug APK and copying to deploy path"
+    DEPENDS ${PROJECT}
+)
+
 if(ANDROID_BUILD_PLAY)
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         set(_gradle_suffix "Debug")
     else()
         set(_gradle_suffix "Release")
     endif()
-    set(_android_build_dir "${CMAKE_CURRENT_BINARY_DIR}/android-build")
     add_custom_target(android_play_apk
-        COMMAND ./gradlew assemblePlay${_gradle_suffix}         WORKING_DIRECTORY "${_android_build_dir}"
+        COMMAND ./gradlew assemblePlay${_gradle_suffix}
+        WORKING_DIRECTORY "${_android_build_dir}"
         COMMENT "Building Android Play APK (assemblePlay${_gradle_suffix})"
         DEPENDS ${PROJECT}
     )
     add_custom_target(android_play_aab
-        COMMAND ./gradlew bundlePlay${_gradle_suffix}         WORKING_DIRECTORY "${_android_build_dir}"
+        COMMAND ./gradlew bundlePlay${_gradle_suffix}
+        WORKING_DIRECTORY "${_android_build_dir}"
         COMMENT "Building Android Play AAB (bundlePlay${_gradle_suffix})"
         DEPENDS ${PROJECT}
     )
