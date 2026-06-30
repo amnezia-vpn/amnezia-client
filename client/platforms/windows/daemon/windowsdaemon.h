bcc1c34ad8b8e11c8716afee8ddc036616723462
cd-amn <cd@amnezia.org>
2026-06-30 19:16:14 +0400

    refactor: clarify Windows legacy tunnel-service cleanup naming

--- client/platforms/windows/daemon/windowsdaemon.h
+++ client/platforms/windows/daemon/windowsdaemon.h
@@ -14,7 +14,7 @@
 #include "windowstunnelservice.h"
 #include "wireguardutilswindows.h"
 
-#define TUNNEL_SERVICE_NAME L"AmneziaWGTunnel$AmneziaVPN"
+#define LEGACY_TUNNEL_SERVICE_NAME L"AmneziaWGTunnel$AmneziaVPN"
 
 class WindowsDaemon final : public Daemon {
   Q_DISABLE_COPY_MOVE(WindowsDaemon)
