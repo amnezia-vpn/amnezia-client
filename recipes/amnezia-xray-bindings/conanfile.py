5e70eb20c9ce38b76ca64c32d98847eb07987237
yyy-amnezia <yyy@amnezia.org>
2026-07-09 08:37:46 +0300

    feat: build macOS prebuilts for arm64 and x86_64 (#2601)
    * Build macOS prebuilts for arm64 and x86_64
    
    * chore: remove parallel thingy from CI/CD
    
    * feat: rework amnezia-xray-bindings for multiarch
    
    * Fix macOS universal recipe builds
    
    * Sign macOS OpenVPN helper script
    
    * Address macOS build review comments
    
    * Isolate awg-go universal builds
    
    * Disable Xcode signing for non-NE macOS
    
    * Address remaining macOS recipe review comments
    
    * feat(conan): rework go-based recipes to support Apple's multiarch
    
    * fix(conan): amnezia-xray-bindings naming conventions
    
    * chore(ci/cd): specify macos versions explicitly
    
    * fix(conan): use escaped path for make in windows
    
    * fix(ci/cd): checkout prior using of provisioning profiles
    
    ---------
    
    Co-authored-by: Yaroslav Gurov <ygurov@proton.me>

--- recipes/amnezia-xray-bindings/conanfile.py
+++ recipes/amnezia-xray-bindings/conanfile.py
@@ -3,13 +3,24 @@ from conan.tools.files import get, copy, collect_libs, chdir, rename
 from conan.tools.layout import basic_layout
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.apple import XCRun, is_apple_os
+from conan.tools.apple.apple import _to_apple_arch
+from conan.tools.env import Environment
 
 import os
+import shlex
+
 
 class AmneziaXrayBindings(ConanFile):
     name = "amnezia-xray-bindings"
     version = "1.1.0"
     settings = "os", "arch", "compiler"
+
+    _arch_map = {
+        "x86": "386",
+        "x86_64": "amd64",
+        "armv8": "arm64"
+    }
 
     @property
     def _goos(self):
@@ -19,15 +30,15 @@ class AmneziaXrayBindings(ConanFile):
             "Macos": "darwin",
             "Windows": "windows"
         }.get(str(self.settings.os))
-    
+
     @property
-    def _goarch(self):
-        return {
-            "x86": "386",
-            "x86_64": "amd64",
-            "armv8": "arm64"
-        }.get(str(self.settings.arch))
-    
+    def _archs(self):
+        return str(self.settings.arch).split("|")
+
+    @property
+    def _is_multiarch(self):
+        return len(self._archs) > 1
+
     @property
     def _is_windows(self):
         return str(self.settings.os).startswith("Windows")
@@ -54,9 +65,14 @@ class AmneziaXrayBindings(ConanFile):
             self.tool_requires("mingw-builds/15.1.0")
 
     def validate(self):
-        if not self._goos or not self._goarch:
+        if not self._goos or not all(arch in self._arch_map for arch in self._archs):
             raise ConanInvalidConfiguration(
                 f"{self.name} v{self.version} does not support {self.settings.os} {self.settings.arch}"
+            )
+
+        if self._is_multiarch and not is_apple_os(self):
+            raise ConanInvalidConfiguration(
+                f"{self.name} v{self.version} does not support multiarch builds"
             )
 
     def source(self):
@@ -65,44 +81,64 @@ class AmneziaXrayBindings(ConanFile):
 
     def generate(self):
         tc = AutotoolsToolchain(self)
-        tc.make_args = [
-            "LIB_ARC=libamnezia_xray.a"
-        ]
+        tc.apple_arch_flag = None
         env = tc.environment()
-        env.define("ARCH", self._goarch)
-        env.define("GOARCH", self._goarch)
+        env.define("GOPATH", os.path.join(self.build_folder, "gopath"))
+        env.define("GOMODCACHE", os.path.join(self.build_folder, "gopath", "pkg", "mod"))
+        env.define("GOCACHE", os.path.join(self.build_folder, "gocache"))
         env.define("GOOS", self._goos)
-        env.define("CGO_LDFLAGS", tc.ldflags)
-        env.define("CGO_CFLAGS", tc.cflags)
         if self._is_windows:
             env.define("OS", "windows")
+        self._ldflags = tc.ldflags
+        self._cflags = tc.cflags
         tc.generate(env)
 
     def build(self):
         with chdir(self, self.source_folder):
-            autotools = Autotools(self)
-            autotools.make()
+            for arch in self._archs:
+                build_dir = os.path.join(self.build_folder, arch) if self._is_multiarch else self.build_folder
+                goarch = self._arch_map.get(arch)
+
+                cflags = list(self._cflags)
+                ldflags = list(self._ldflags)
+                if is_apple_os(self):
+                    cflags.append(f"-arch {_to_apple_arch(arch)}")
+                    ldflags.append(f"-arch {_to_apple_arch(arch)}")
+
+                env = Environment()
+                env.define("ARCH", goarch)
+                env.define("CGO_CFLAGS", " ".join(cflags))
+                env.define("CGO_LDFLAGS", " ".join(ldflags))
+                with env.vars(self).apply():
+                    at = Autotools(self)
+                    at.make(args=[
+                        f"BUILD_DIR={build_dir.replace("\\", "/") if self._is_windows else build_dir}"
+                    ])
+
+            if is_apple_os(self) and self._is_multiarch:
+                lipo = XCRun(self).find('lipo')
+                archives = [os.path.join(self.build_folder, arch, "amnezia_xray.a") for arch in self._archs]
+                output = os.path.join(self.build_folder, "amnezia_xray.a")
+                self.run("{} -create -output {} {}".format(
+                    shlex.quote(lipo),
+                    shlex.quote(output),
+                    shlex.join(archives)
+                ))
+
+                copy(self, "*.h", os.path.join(self.build_folder, self._archs[0]), self.build_folder)
 
     def _rename_header(self):
         if not self._is_windows:
-            rename(self, os.path.join(self.package_folder, "include", "libamnezia_xray.h"),
-                    os.path.join(self.package_folder, "include", "amnezia_xray.h"))
-
-    def _rename_libs(self):
-        # workaround of bad naming strategy in amnezia-xray-bindings
-        # TODO: change it and kick out the code below
-        lib_dir = os.path.join(self.package_folder, "lib")
-        for fname in os.listdir(lib_dir):
-            if not fname.startswith("lib"):
-                src = os.path.join(lib_dir, fname)
-                dst = os.path.join(lib_dir, "lib" + fname)
-                os.rename(src, dst)
+            rename(self,
+                os.path.join(self.package_folder, "lib", "amnezia_xray.a"),
+                os.path.join(self.package_folder, "lib", "libamnezia_xray.a")
+            )
 
     def package(self):
-        copy(self, "*.h", src=self.build_folder, dst=os.path.join(self.package_folder, "include"))
-        copy(self, "*.a", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"))
-        copy(self, "*.lib", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"))
-        copy(self, "*.dll", src=self.build_folder, dst=os.path.join(self.package_folder, "bin"))
+        copy(self, "amnezia_xray.h", src=self.build_folder, dst=os.path.join(self.package_folder, "include"), keep_path=False)
+        copy(self, "amnezia_xray.a", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "amnezia_xray.lib", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "amnezia_xray.dll", src=self.build_folder, dst=os.path.join(self.package_folder, "bin"), keep_path=False)
         self._rename_header()
 
     def package_info(self):
