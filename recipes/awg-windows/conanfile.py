97fd853fd452b2b69b5fdef71e25e16a713018a0
Yaroslav Gurov <ygurov@proton.me>
2026-07-20 00:56:32 +0200

    chore: update awg-windows checksum

--- recipes/awg-windows/conanfile.py
+++ recipes/awg-windows/conanfile.py
@@ -63,7 +63,7 @@ class AwgWindows(ConanFile):
 
     def source(self):
         get(self, f"https://github.com/amnezia-vpn/amneziawg-windows/archive/refs/tags/v{self.version}.zip",
-            sha256="5c29a75cb2beae291cc51b64840a39f838da5f300b9e956f7964813a687ec74c", strip_root=True)
+            sha256="9e74240b9bae2f9c8a9381bdf6e5a43ad37653de186e01e0f5a92935e4310252", strip_root=True)
         
     def generate(self):
         tc = AutotoolsToolchain(self)
